914009c55e64de472ab402ab47d7537ab3d8ecef
Federico Menozzi <federicogmenozzi@gmail.com>
2019-06-20 21:28:47 -0700

    Implement basic controller remapping.
    There are a couple of edge cases to iron out, such as handling joystick
    inputs and removing previous mappings for other actions if they match
    the new one. Also, controls should be saved and loaded from disk so that
    the player doesn't have to reconfigure them every time they start the
    game.
    
    For completeness, the 'player_interact' input action was added, though
    it currently does not do anything.

--- ui/menus/options/controller/controller_options_menu.gd
+++ ui/menus/options/controller/controller_options_menu.gd
@@ -5,7 +5,73 @@ onready var _attack_button: Button = $Attack/Attack
 onready var _dash_button: Button = $Dash/Dash
 onready var _interact_button: Button = $Interact/Interact
 
+onready var _jump_texture_button: TextureButton = $Jump/JumpButtonTexture
+onready var _attack_texture_button: TextureButton = $Attack/AttackButtonTexture
+onready var _dash_texture_button: TextureButton = $Dash/DashButtonTexture
+onready var _interact_texture_button: TextureButton = $Interact/InteractButtonTexture
+
 onready var _back_button: Button = $Back
+
+enum PlayerAction {
+	JUMP,
+	ATTACK,
+	DASH,
+	INTERACT,
+}
+
+onready var PLAYER_ACTION_DATA: Dictionary = {
+	PlayerAction.JUMP: {
+		'ui_button':         _jump_button,
+		'ui_texture_button': _jump_texture_button,
+		'ui_button_text':    'Jump',
+		'input_action':      'player_jump',
+	},
+	PlayerAction.ATTACK: {
+		'ui_button':         _attack_button,
+		'ui_texture_button': _attack_texture_button,
+		'ui_button_text':    'Attack',
+		'input_action':      'player_attack',
+	},
+	PlayerAction.DASH: {
+		'ui_button':         _dash_button,
+		'ui_texture_button': _dash_texture_button,
+		'ui_button_text':    'Dash',
+		'input_action':      'player_dash',
+	},
+	PlayerAction.INTERACT: {
+		'ui_button':         _interact_button,
+		'ui_texture_button': _interact_texture_button,
+		'ui_button_text':    'Interact',
+		'input_action':      'player_interact',
+	},
+}
+
+# Used to keep track of when input events correspond to navigating the menu vs
+# actually remapping the controls.
+var _is_remapping: bool = false
+
+# UI button corresponding to the control that we want to remap.
+var _player_action_to_remap: int = -1
+
+const JOYPAD_BUTTONS_TO_TEXTURES: Dictionary = {
+	# Main buttons.
+	JOY_XBOX_A: preload('res://assets/xbox-a.png'),
+	JOY_XBOX_B: preload('res://assets/xbox-b.png'),
+	JOY_XBOX_X: preload('res://assets/xbox-x.png'),
+	JOY_XBOX_Y: preload('res://assets/xbox-y.png'),
+
+	# Bumbers and triggers.
+	JOY_L:  preload('res://assets/xbox-lb.png'),
+	JOY_R:  preload('res://assets/xbox-rb.png'),
+	JOY_L2: preload('res://assets/xbox-lt.png'),
+	JOY_R2: preload('res://assets/xbox-rt.png'),
+
+	# D-pad.
+	JOY_DPAD_UP:    preload('res://assets/xbox-dpad-up.png'),
+	JOY_DPAD_RIGHT: preload('res://assets/xbox-dpad-right.png'),
+	JOY_DPAD_DOWN:  preload('res://assets/xbox-dpad-down.png'),
+	JOY_DPAD_LEFT:  preload('res://assets/xbox-dpad-left.png'),
+}
 
 func enter(pause: Pause, previous_menu: int) -> void:
 	self.visible = true
@@ -22,22 +88,74 @@ func exit(pause: Pause) -> void:
 	self.visible = false
 
 func handle_input(pause: Pause, event: InputEvent) -> void:
-	if event.is_action_pressed('ui_pause'):
-		change_menu(pause.Menu.CONTROLLER_OPTIONS, pause.Menu.UNPAUSED)
-	elif event.is_action_pressed('ui_cancel'):
-		change_menu(pause.Menu.CONTROLLER_OPTIONS, pause.Menu.OPTIONS)
+	if _is_remapping and event is InputEventJoypadButton:
+		remap(event)
+		_is_remapping = false
+	else:
+		if event.is_action_pressed('ui_pause'):
+			change_menu(pause.Menu.CONTROLLER_OPTIONS, pause.Menu.UNPAUSED)
+		elif event.is_action_pressed('ui_cancel'):
+			change_menu(pause.Menu.CONTROLLER_OPTIONS, pause.Menu.OPTIONS)
+
+func setup_remap(player_action: int) -> void:
+	# Change button text.
+	var ui_button = PLAYER_ACTION_DATA[player_action]['ui_button']
+	ui_button.set_text('Press new...')
+
+	# TODO: Maybe yield on some kind of input signal so that we can do the
+	#       remapping here and don't have to maintain a separate _is_remapping
+	#       state and handle remapping in handle_input().
+
+	_is_remapping = true
+	_player_action_to_remap = player_action
+
+# TODO: Have this persist somewhere.
+func remap(event: InputEventJoypadButton) -> void:
+	# Consume input event so that it doesn't get interpreted as e.g. a
+	# button click/press immediately afterwards.
+	get_tree().set_input_as_handled()
+
+	var current_action_data = PLAYER_ACTION_DATA[_player_action_to_remap]
+
+	# Make sure the desired remapping is allowed.
+	var button_index = event.get_button_index()
+	if not JOYPAD_BUTTONS_TO_TEXTURES.has(button_index):
+		# Reset UI button text.
+		var ui_button = current_action_data['ui_button']
+		ui_button.set_text(current_action_data['ui_button_text'])
+
+		return
+
+	# Remove previous mappings and add the new one.
+	#
+	# TODO: Also remove previous mappings in other buttons if they match the
+	#       new one. InputMap.get_action_list(action: String) could be useful
+	#       for this.
+	#
+	# TODO: Maybe allow two mappings at once instead of just one?
+	var action = current_action_data['input_action']
+	InputMap.action_erase_events(action)
+	InputMap.action_add_event(action, event)
+
+	# Set new texture.
+	var texture_button = current_action_data['ui_texture_button']
+	texture_button.set_texture(JOYPAD_BUTTONS_TO_TEXTURES[button_index])
+
+	# Reset UI button text.
+	var ui_button = current_action_data['ui_button']
+	ui_button.set_text(current_action_data['ui_button_text'])
 
 func _on_jump_pressed(pause: Pause) -> void:
-	print('jump remapping not yet implemented')
+	setup_remap(PlayerAction.JUMP)
 
 func _on_attack_pressed(pause: Pause) -> void:
-	print('attack remapping not yet implemented')
+	setup_remap(PlayerAction.ATTACK)
 
 func _on_dash_pressed(pause: Pause) -> void:
-	print('dash remapping not yet implemented')
+	setup_remap(PlayerAction.DASH)
 
 func _on_interact_pressed(pause: Pause) -> void:
-	print('interact remapping not yet implemented')
+	setup_remap(PlayerAction.INTERACT)
 
 func _on_back_pressed(pause: Pause) -> void:
 	change_menu(pause.Menu.CONTROLLER_OPTIONS, pause.Menu.OPTIONS)
