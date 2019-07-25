20b951e64665fb62e55e1f9fa6e547d39ce5f00d
Federico Menozzi <federicogmenozzi@gmail.com>
2019-07-24 19:26:57 -0700

    Remove dead code in room.gd.

--- rooms/long_hallway.gd
+++ rooms/long_hallway.gd
@@ -1,4 +1,1 @@
 extends 'res://rooms/room.gd'
-
-func _ready() -> void:
-    .setup_room($RoomBoundaries)
--- rooms/room.gd
+++ rooms/room.gd
@@ -1,36 +1,6 @@
 extends Node2D
 class_name Room
 
-# Determines how the camera will behave in each room.
-#
-# TODO: Do we still need this?
-enum CameraBehavior {
-    # For small rooms that fit entirely within the viewport. When the player
-    # enters a ROOM_FIXED room, the camera will be fixed to the center of the
-    # viewport (i.e. the center of the room).
-    ROOM_FIXED,
-
-    # For rooms whose width fits within the viewport. When the player enters a
-    # ROOM_FIXED_VERTICAL room, the camera will follow the player up and down
-    # but there will be no camera motion left and right.
-    ROOM_FIXED_VERTICAL,
-
-    # For rooms whose height fits within the viewport. When the player enters a
-    # ROOM_FIXED_HORIZONTAL room, the camera will follow the player left and
-    # right but there will be no camera motion up and down.
-    ROOM_FIXED_HORIZONTAL,
-
-    # For large rooms that do not fit entirely within the viewport. When the
-    # player enters a PLAYER_FIXED room, the camera will be centered on the
-    # player and will follow them around accordingly.
-    PLAYER_FIXED,
-}
-export(CameraBehavior) var camera_behavior := CameraBehavior.ROOM_FIXED
-
-# TODO: Do we still need this?
-func setup_room(room_boundaries: Area2D) -> void:
-    pass
-
 # Get global positions of all camera anchors in each room. During a transition,
 # the player camera will interpolate its position from the closest anchor in
 # the old room to the closest anchor in the new room.
