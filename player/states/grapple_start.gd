9435fa456a97117b4645dff78054eaf23d57e3a5
Federico Menozzi <federicogmenozzi@gmail.com>
2019-07-27 18:29:10 -0700

    Slightly improve grapple start animations.
    The rope now fades in and out during the animations.

--- player/states/grapple_start.gd
+++ player/states/grapple_start.gd
@@ -1,6 +1,12 @@
 extends "res://scripts/state.gd"
 
+# Reference to the player. Needed for draw_grapple_rope() and
+# clear_grapple_rope(), which are called from the animation player.
+var _player: Player = null
+
 func enter(player: Player, previous_state_dict: Dictionary) -> void:
+    _player = player
+
     if player.is_on_ground():
         player.get_animation_player().play('grapple_launch_long')
     else:
@@ -12,13 +18,8 @@ func enter(player: Player, previous_state_dict: Dictionary) -> void:
     var grapple_direction := sign((grapple_point_pos - player.global_position).x)
     player.set_player_direction(grapple_direction)
 
-    # Draw grapple rope from player to grapple attachment point.
-    var grapple_rope := player.get_grapple_rope()
-    grapple_rope.add_point(Vector2.ZERO)
-    grapple_rope.add_point(grapple_point_pos - grapple_rope.global_position)
-
 func exit(player: Player) -> void:
-    player.get_grapple_rope().clear_points()
+    pass
 
 func handle_input(player: Player, event: InputEvent) -> Dictionary:
     return {'new_state': player.State.NO_CHANGE}
@@ -29,3 +30,13 @@ func update(player: Player, delta: float) -> Dictionary:
         return {'new_state': player.State.GRAPPLE}
 
     return {'new_state': player.State.NO_CHANGE}
+
+func draw_grapple_rope() -> void:
+    var grapple_point_pos := _player.get_next_grapple_point().get_attachment_pos()
+
+    var grapple_rope := _player.get_grapple_rope()
+    grapple_rope.add_point(Vector2.ZERO)
+    grapple_rope.add_point(grapple_point_pos - grapple_rope.global_position)
+
+func clear_grapple_rope() -> void:
+    _player.get_grapple_rope().clear_points()
