5ddcaf87d4f75f29f59f015fdc9aff56930006d2
Federico Menozzi <federicogmenozzi@gmail.com>
2019-09-07 12:12:24 -0700

    Add a 'Read' prompt when approaching signs.

--- sign.gd
+++ sign.gd
@@ -3,10 +3,16 @@ extends Area2D
 export(Array, String) var dialog
 
 onready var _sprite: Sprite = $Sprite
-onready var _tween: Tween = $OutlineTween
+onready var _outline_tween: Tween = $Sprite/OutlineTween
 
-const UNHIGHLIGHTED := Color.white
-const HIGHLIGHTED := Color(10, 10, 10, 10)
+onready var _label: Label = $Label
+onready var _label_tween: Tween = $Label/LabelTween
+
+const SIGN_UNHIGHLIGHTED := Color.white
+const SIGN_HIGHLIGHTED := Color(10, 10, 10, 10)
+
+const LABEL_VISIBLE := Color(1, 1, 1, 1)
+const LABEL_NOT_VISIBLE := Color(1, 1, 1, 0)
 
 func _ready() -> void:
     self.connect('body_entered', self, '_on_player_entered')
@@ -18,7 +24,8 @@ func _on_player_entered(player: Player) -> void:
 
     player.set_nearby_sign(self)
 
-    _modulate_color(UNHIGHLIGHTED, HIGHLIGHTED)
+    _modulate_sign_color(SIGN_UNHIGHLIGHTED, SIGN_HIGHLIGHTED)
+    _modulate_label_visibility(LABEL_NOT_VISIBLE, LABEL_VISIBLE)
 
 func _on_player_exited(player: Player) -> void:
     if not player:
@@ -26,14 +33,27 @@ func _on_player_exited(player: Player) -> void:
 
     player.set_nearby_sign(null)
 
-    _modulate_color(HIGHLIGHTED, UNHIGHLIGHTED)
+    _modulate_sign_color(SIGN_HIGHLIGHTED, SIGN_UNHIGHLIGHTED)
+    _modulate_label_visibility(LABEL_VISIBLE, LABEL_NOT_VISIBLE)
 
-func _modulate_color(old: Color, new: Color) -> void:
+func _modulate_sign_color(old: Color, new: Color) -> void:
     var prop := 'modulate'
     var duration := 0.25
     var trans := Tween.TRANS_QUAD
     var easing := Tween.EASE_IN
 
-    _tween.stop_all()
-    _tween.interpolate_property(self, prop, old, new, duration, trans, easing)
-    _tween.start()
+    var tween := _outline_tween
+    tween.stop_all()
+    tween.interpolate_property(self, prop, old, new, duration, trans, easing)
+    tween.start()
+
+func _modulate_label_visibility(old: Color, new: Color) -> void:
+    var prop := 'modulate'
+    var duration := 0.25
+    var trans := Tween.TRANS_QUAD
+    var easing := Tween.EASE_IN
+
+    var tween := _label_tween
+    tween.stop_all()
+    tween.interpolate_property(_label, prop, old, new, duration, trans, easing)
+    tween.start()
