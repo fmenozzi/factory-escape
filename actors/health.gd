daf67f8be88c067e887c74a52a0432657e12641f
Federico Menozzi <federicogmenozzi@gmail.com>
2019-09-21 15:29:20 -0700

    Add died signal to Health node.

--- actors/health.gd
+++ actors/health.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal health_changed(old_health, new_health)
+signal died
 
 export(int) var MAX_HEALTH = 5
 
@@ -26,6 +27,8 @@ func take_damage(damage_amount: int) -> void:
     _current_health = new_health
 
     emit_signal('health_changed', old_health, new_health)
+    if new_health == 0:
+        emit_signal('died')
 
 func heal(heal_amount: int) -> void:
     var old_health := _current_health
--- game.gd
+++ game.gd
@@ -4,5 +4,9 @@ onready var player: Player = $World/Player
 onready var health_bar: Control = $UILayer/Healthbar
 
 func _ready() -> void:
-    player.get_health().connect(
-        'health_changed', health_bar, '_on_health_changed')
+    var player_health := player.get_health()
+    player_health.connect('health_changed', health_bar, '_on_health_changed')
+    player_health.connect('died', self, '_on_player_died')
+
+func _on_player_died() -> void:
+    print('YOU DIED')
