7203158d981f4966ab204a028ebd4493cca08e2d
Federico Menozzi <federicogmenozzi@gmail.com>
2019-10-13 10:03:55 -0700

    Allow for jumping/dashing/grappling/attacking during HAZARD_RECOVER.

--- actors/player/states/hazard_recover.gd
+++ actors/player/states/hazard_recover.gd
@@ -3,10 +3,32 @@ extends "res://scripts/state.gd"
 func enter(player: Player, previous_state_dict: Dictionary) -> void:
     player.get_animation_player().play('hazard_recover')
 
+    # Reset the dash and double jump.
+    player.reset_dash()
+    player.reset_jump()
+
 func exit(player: Player) -> void:
     pass
 
 func handle_input(player: Player, event: InputEvent) -> Dictionary:
+    if event.is_action_pressed('player_jump') and player.can_jump():
+        return {'new_state': Player.State.JUMP}
+    elif event.is_action_pressed('player_attack'):
+        # Play attack animation before returning to idle animation.
+        player.start_attack()
+        player.get_animation_player().queue('idle')
+    elif event.is_action_pressed('player_dash') and player.can_dash():
+        # Only dash if the cooldown is done.
+        if player.get_dash_cooldown_timer().is_stopped():
+            return {'new_state': Player.State.DASH}
+    elif event.is_action_pressed('player_grapple'):
+        var next_grapple_point := player.get_next_grapple_point()
+        if next_grapple_point != null:
+            return {
+                'new_state': Player.State.GRAPPLE_START,
+                'grapple_point': next_grapple_point,
+            }
+
     return {'new_state': Player.State.NO_CHANGE}
 
 func update(player: Player, delta: float) -> Dictionary:
