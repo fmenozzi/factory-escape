978b6113cf34a2cd5538e646608ac6c14b5d4c71
Federico Menozzi <federicogmenozzi@gmail.com>
2019-10-19 10:48:46 -0700

    Allow slime enemies to take hits and die.

--- actors/enemies/slime.gd
+++ actors/enemies/slime.gd
@@ -4,6 +4,7 @@ export(Util.Direction) var direction := Util.Direction.RIGHT
 
 const SPEED := 0.25 * Util.TILE_SIZE
 
+onready var _health: Health = $Health
 onready var _hurtbox: Area2D = $Hurtbox
 onready var _edge_raycast_left: RayCast2D = $LedgeDetectorRaycasts/Left
 onready var _edge_raycast_right: RayCast2D = $LedgeDetectorRaycasts/Right
@@ -11,6 +12,11 @@ onready var _edge_raycast_right: RayCast2D = $LedgeDetectorRaycasts/Right
 func _ready() -> void:
     $AnimationPlayer.play('walk')
     _set_direction(direction)
+
+    _hurtbox.connect('area_entered', self, '_on_hit_taken')
+
+    _health.connect('health_changed', self, '_on_health_changed')
+    _health.connect('died', self, '_on_died')
 
 func _physics_process(delta: float) -> void:
     if is_on_wall() or _is_touching_hazard() or _is_near_ledge():
@@ -37,3 +43,19 @@ func _is_near_ledge() -> bool:
     var near_right := not _edge_raycast_right.is_colliding()
 
     return (near_left and not near_right) or (near_right and not near_left)
+
+func _on_hit_taken(hitbox: Area2D) -> void:
+    if Util.in_collision_layer(hitbox, ['hazards']):
+        return
+
+    # TODO: Do we need to use the RID method to ensure no hits get
+    #       double-counted? Is there a way to automatically detect whether this
+    #       has happened in the meantime?
+    _health.take_damage(1)
+
+func _on_health_changed(old_health: int, new_health: int) -> void:
+    print('SLIME HIT (new health: ', new_health, ')')
+
+func _on_died() -> void:
+    print('SLIME DIED')
+    queue_free()
