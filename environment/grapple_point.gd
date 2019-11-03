eb47cc3acc52a8748bae91819553bce50f5afe19
Federico Menozzi <federicogmenozzi@gmail.com>
2019-11-03 10:23:24 -0800

    Cache references to nodes in grapple_point.gd.

--- environment/grapple_point.gd
+++ environment/grapple_point.gd
@@ -14,28 +14,37 @@ export(GrappleType) var grapple_type := GrappleType.NORMAL
 
 var _available: bool = true
 
+onready var _attachment_point: Position2D = $AttachmentPoint
+onready var _grapple_range_area: Area2D = $GrappleRangeArea
+onready var _no_grapple_area: Area2D = $NoGrappleArea
+
+onready var _launch_point_left: Position2D = $LaunchGrapplePoints/Left
+onready var _launch_point_right: Position2D = $LaunchGrapplePoints/Right
+
+onready var _visibility_notifier: VisibilityNotifier2D = $VisibilityNotifier2D
+
 func _ready() -> void:
     $AnimationPlayer.play('shimmer')
 
 func get_attachment_pos() -> Position2D:
-    return $AttachmentPoint as Position2D
+    return _attachment_point
 
 func get_grapple_range_area() -> Area2D:
-    return $GrappleRangeArea as Area2D
+    return _grapple_range_area
 
 func get_no_grapple_area() -> Area2D:
-    return $NoGrappleArea as Area2D
+    return _no_grapple_area
 
 func get_grapple_type() -> int:
     return grapple_type
 
 func get_launch_grapple_points() -> Array:
-    return [$LaunchGrapplePoints/Left, $LaunchGrapplePoints/Right]
+    return [_launch_point_left, _launch_point_right]
 
 func is_on_screen() -> bool:
     # TODO: This doesn't work sometimes, likely because the granularity of the
     #       grid used for doing these calculations is too small.
-    return $VisibilityNotifier2D.is_on_screen()
+    return _visibility_notifier.is_on_screen()
 
 # Grapple points can be marked as unavailable to be excluded from consideration
 # during grapple point selection.
