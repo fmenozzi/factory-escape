7ea3031c556f4e57256ecc8c9cb4e18f350a255d
Federico Menozzi <federicogmenozzi@gmail.com>
2019-11-03 10:28:37 -0800

    Cache references to HealthNodes in health_bar.gd.

--- ui/interface/health_bar.gd
+++ ui/interface/health_bar.gd
@@ -3,18 +3,19 @@ extends Control
 const EMPTY_HEALTH_TEXTURE := preload('res://assets/health-node-empty.png')
 const FULL_HEALTH_TEXTURE := preload('res://assets/health-node-full.png')
 
-onready var NUM_HEALTH_NODES: int = $HealthNodes.get_child_count()
+onready var _health_nodes: VBoxContainer = $HealthNodes
+onready var NUM_HEALTH_NODES: int = _health_nodes.get_child_count()
 
 var _current_health: int = 5
 
 func set_health(new_health: int) -> void:
     assert new_health <= NUM_HEALTH_NODES
 
-    for health_node in $HealthNodes.get_children():
+    for health_node in _health_nodes.get_children():
         health_node.texture = EMPTY_HEALTH_TEXTURE
 
     for idx in range(new_health):
-        $HealthNodes.get_child(idx).texture = FULL_HEALTH_TEXTURE
+        _health_nodes.get_child(idx).texture = FULL_HEALTH_TEXTURE
 
 func _on_health_changed(old_health: int, new_health: int) -> void:
     set_health(new_health)
