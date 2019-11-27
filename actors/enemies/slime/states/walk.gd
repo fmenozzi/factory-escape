a7ca4577801c4347132e97e8aa6d25b62497e2dd
Federico Menozzi <federicogmenozzi@gmail.com>
2019-11-27 08:55:53 -0800

    Add slime state machine skeleton code.

---
+++ actors/enemies/slime/states/walk.gd
@@ -0,0 +1,10 @@
+extends "res://actors/enemies/state.gd"
+
+func enter(slime, previous_state_dict: Dictionary) -> void:
+    pass
+
+func exit(slime) -> void:
+    pass
+
+func update(slime, delta: float) -> Dictionary:
+    return {}
