b94dc7387fe936774fc2a0909233121d0f3a4a23
Federico Menozzi <federicogmenozzi@gmail.com>
2019-11-27 17:44:11 -0800

    Rename 'host' to 'player' in player state script.

--- actors/player/states/state.gd
+++ actors/player/states/state.gd
@@ -1,20 +1,20 @@
 extends Node
 
-# Called when host enters this state. The metadata data of the previous state is
-# also passed in.
-func enter(host, previous_state_dict: Dictionary) -> void:
+# Called when the player enters this state. The metadata data of the previous
+# state is also passed in.
+func enter(player: Player, previous_state_dict: Dictionary) -> void:
     pass
 
-# Called when host exits this state.
-func exit(host) -> void:
+# Called when the player exits this state.
+func exit(player: Player) -> void:
     pass
 
-# Called when handling input from the host.
+# Called when handling input from the player.
 #
 # Returns a dictionary with at least the new state to transition to (in the
 # 'new_state' key). If the new state is NO_CHANGE, remain in current state. Can
 # also pass additional metadata via other keys.
-func handle_input(host, event: InputEvent) -> Dictionary:
+func handle_input(player: Player, event: InputEvent) -> Dictionary:
     return {}
 
 # Called on every frame.
@@ -22,5 +22,5 @@ func handle_input(host, event: InputEvent) -> Dictionary:
 # Returns a dictionary with at least the new state to transition to (in the
 # 'new_state' key). If the new state is NO_CHANGE, remain in current state. Can
 # also pass additional metadata via other keys.
-func update(host, delta: float) -> Dictionary:
+func update(player: Player, delta: float) -> Dictionary:
     return {}
