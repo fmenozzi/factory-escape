c3772360d5555466f48a4758071e54cc5907edf1
Federico Menozzi <federicogmenozzi@gmail.com>
2019-11-29 07:17:47 -0800

    Remove flash shader from FlashManager's sprite on flash timeout.

--- actors/flash_manager.gd
+++ actors/flash_manager.gd
@@ -10,6 +10,7 @@ export(float) var total_duration := 2.0
 export(float) var single_flash_duration := 0.075
 export(Color) var flash_color := Color.white
 
+onready var _sprite: Sprite = null
 onready var _timer: Timer = $Timer
 onready var _tween: Tween = $Tween
 
@@ -24,9 +25,13 @@ func _ready() -> void:
     _timer.wait_time = total_duration
     _timer.connect('timeout', self, '_on_flashing_timeout')
 
-    _setup_tween()
+    # Get the sprite to flash.
+    assert(sprite_path != "")
+    _sprite = get_node(sprite_path)
 
 func start_flashing() -> void:
+    _setup_tween()
+
     _timer.start()
     _tween.resume_all()
 func stop_flashing() -> void:
@@ -39,24 +44,21 @@ func resume_timer() -> void:
     _timer.paused = false
 
 func _setup_tween() -> void:
-    # Get the sprite to flash.
-    assert(sprite_path != "")
-    var sprite: Sprite = get_node(sprite_path)
-
     # Attach flash shader to sprite and set flash color shader param.
     var shader_material := ShaderMaterial.new()
     shader_material.set_shader(flash_shader)
     shader_material.set_shader_param('flash_color', flash_color)
-    sprite.set_material(shader_material)
+    _sprite.set_material(shader_material)
 
-    # Setup flash tween to animate lerp amount in flash shader.
-    var material := sprite.get_material()
+    # Reset flash tween to animate lerp amount in flash shader.
+    var material := _sprite.get_material()
     var param := 'shader_param/lerp_amount'
     var old := 0.0
     var new := 1.0
     var trans := Tween.TRANS_LINEAR
     var easing := Tween.EASE_IN
     var delay := single_flash_duration
+    _tween.remove_all()
     _tween.interpolate_property(
         material, param, old, new, single_flash_duration, trans, easing)
     _tween.interpolate_property(
@@ -64,4 +66,9 @@ func _setup_tween() -> void:
 
 func _on_flashing_timeout() -> void:
     stop_flashing()
+
+    # Remove shader from sprite. This is primarily to allow for other shaders to
+    # be used on the sprite afterwards if necessary.
+    _sprite.set_material(null)
+
     emit_signal('flashing_finished')
