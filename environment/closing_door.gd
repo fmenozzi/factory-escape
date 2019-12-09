549962c1e36c13b1e88f5ab6fa23ed3bc84b588b
Federico Menozzi <federicogmenozzi@gmail.com>
2019-12-08 16:44:19 -0800

    Add a small dust puff when closing doors.

--- environment/closing_door.gd
+++ environment/closing_door.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 signal door_opened
 signal door_closed
 
+const DustPuff := preload('res://sfx/LandingPuff.tscn')
+
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 onready var _collision_shape: CollisionShape2D = $CollisionShape2D
 
@@ -18,7 +20,13 @@ func open() -> void:
 func close() -> void:
     _collision_shape.set_deferred('disabled', false)
     _animation_player.play('close')
+
+    # Small dust puff after the door closes.
     yield(_animation_player, 'animation_finished')
+    var dust_puff := DustPuff.instance()
+    dust_puff.show_behind_parent = false
+    Util.spawn_particles(dust_puff, self)
+
     _is_closed = true
     emit_signal('door_closed')
 
