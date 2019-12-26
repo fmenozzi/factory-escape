973ea917e7183d1f846eac5d9bc670e33fad2f43
Federico Menozzi <federicogmenozzi@gmail.com>
2019-12-26 14:37:54 -0800

    Take advantage of now-guaranteed Room structure in room.gd.

--- rooms/room.gd
+++ rooms/room.gd
@@ -1,6 +1,9 @@
 extends Node2D
 class_name Room
 
+onready var _collision_shape: CollisionShape2D = $RoomBoundaries/CollisionShape2D
+onready var _camera_anchors: Array = $CameraAnchors.get_children()
+onready var _grapple_points: Array = $GrapplePoints.get_children()
 onready var _moving_platforms: Array = $MovingPlatforms.get_children()
 
 # Get global positions of all camera anchors in each room. During a transition,
@@ -8,7 +11,7 @@ onready var _moving_platforms: Array = $MovingPlatforms.get_children()
 # the old room to the closest anchor in the new room.
 func get_camera_anchors() -> Array:
     var anchors = []
-    for anchor in get_node('CameraAnchors').get_children():
+    for anchor in _camera_anchors:
         anchors.push_back(anchor.global_position)
     return anchors
 
@@ -29,13 +32,12 @@ func get_closest_camera_anchor(player: Player) -> Vector2:
 # Get all the GrapplePoint nodes in the current room.
 func get_grapple_points() -> Array:
     var grapple_points = []
-    for grapple_point in get_node('GrapplePoints').get_children():
+    for grapple_point in _grapple_points:
         grapple_points.push_back(grapple_point)
     return grapple_points
 
-# TODO: don't rely on inherited node strucutres for getting Area2D.
 func get_room_dimensions() -> Vector2:
-    var half_extents = get_node('RoomBoundaries').get_node('CollisionShape2D').shape.extents
+    var half_extents = _collision_shape.shape.extents
 
     return 2 * half_extents
 
