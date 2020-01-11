a6452755d4a88b2a8f73b2b07f74319e217ae925
Federico Menozzi <federicogmenozzi@gmail.com>
2020-01-11 11:19:08 -0800

    Fade in new lamp label text after 'lit' animation finishes.

--- environment/lamp.gd
+++ environment/lamp.gd
@@ -29,22 +29,31 @@ func _unhandled_input(event: InputEvent) -> void:
                 set_process_unhandled_input(false)
                 _is_lit = true
 
+                # Make sure player is facing lamp.
                 _player.set_direction(Util.direction(_player, self))
 
-                _fade_in_out_label.set_text('Rest')
+                # Fade out label text so that it can be changed and faded back
+                # in.
+                _fade_in_out_label.fade_out()
 
                 # Play light_lamp animation and wait for that to finish before
                 # the lamp actually lights.
                 #
                 # TODO: This might be better served as its own state.
                 var player_animation_player := _player.get_animation_player()
-                _player.get_animation_player().play('light_lamp')
-                yield(_player.get_animation_player(), 'animation_finished')
-                _player.get_animation_player().play('idle')
+                player_animation_player.play('light_lamp')
+                yield(player_animation_player, 'animation_finished')
+                player_animation_player.play('idle')
 
                 _light_sprite.visible = true
                 _animation_player.play('unlit_to_lit')
                 _animation_player.queue('lit')
+
+                # Wait until we've started the 'lit' animation before fading in
+                # new label text.
+                yield(_animation_player, 'animation_started')
+                _fade_in_out_label.set_text('Rest')
+                _fade_in_out_label.fade_in()
 
                 set_process_unhandled_input(true)
 
