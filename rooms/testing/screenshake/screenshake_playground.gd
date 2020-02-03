7dba5ee5ae2bcb4dc5efe41ca84c1ea62aea100f
Federico Menozzi <federicogmenozzi@gmail.com>
2020-02-02 20:33:10 -0800

    Use player_interact to start/stop shake in ScreenshakePlayground.

--- rooms/testing/screenshake/screenshake_playground.gd
+++ rooms/testing/screenshake/screenshake_playground.gd
@@ -1,10 +1,14 @@
 extends Room
 
 onready var _player: Player = $Player
+onready var _camera: Camera2D = _player.get_camera()
+onready var _screenshake: Node2D = _camera.get_node('Screenshake')
 
 onready var _duration_options: OptionButton = $OptionButtons/DurationOptions
 onready var _frequency_options: OptionButton = $OptionButtons/FrequencyOptions
 onready var _amplitude_options: OptionButton = $OptionButtons/AmplitudeOptions
+
+var _is_shaking := false
 
 func _ready() -> void:
     _duration_options.add_item('duration short', 0)
@@ -17,37 +21,41 @@ func _ready() -> void:
     _amplitude_options.add_item('amplitude medium', 1)
     _amplitude_options.add_item('amplitude large', 2)
 
+    _screenshake.connect('started_shaking', self, '_on_started_shaking')
+    _screenshake.connect('stopped_shaking', self, '_on_stopped_shaking')
+
 func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed('ui_up'):
-        _start_shaking()
-    if event.is_action_pressed('ui_down'):
-        _stop_shaking()
+    if event.is_action_pressed('player_interact'):
+        if _is_shaking:
+            _stop_shaking()
+        else:
+            _start_shaking()
 
 func _start_shaking() -> void:
-    var camera := _player.get_camera()
-    var screenshake := camera.get_node('Screenshake')
-
     var duration := 0.0
     match _duration_options.selected:
         0:
-            duration = screenshake.SHAKE_DURATION_SHORT
+            duration = _screenshake.SHAKE_DURATION_SHORT
         1:
-            duration = screenshake.SHAKE_DURATION_MEDIUM
+            duration = _screenshake.SHAKE_DURATION_MEDIUM
         2:
-            duration = screenshake.SHAKE_DURATION_LONG
+            duration = _screenshake.SHAKE_DURATION_LONG
 
     var amplitude := 0.0
     match _amplitude_options.selected:
         0:
-            amplitude = screenshake.SHAKE_AMPL_SMALL
+            amplitude = _screenshake.SHAKE_AMPL_SMALL
         1:
-            amplitude = screenshake.SHAKE_AMPL_MEDIUM
+            amplitude = _screenshake.SHAKE_AMPL_MEDIUM
         2:
-            amplitude = screenshake.SHAKE_AMPL_LARGE
+            amplitude = _screenshake.SHAKE_AMPL_LARGE
 
-    camera.shake(duration, screenshake.SHAKE_FREQ, amplitude)
+    _camera.shake(duration, _screenshake.SHAKE_FREQ, amplitude)
 
 func _stop_shaking() -> void:
-    var camera := _player.get_camera()
-    var screenshake := camera.get_node('Screenshake')
-    screenshake.stop()
+    _screenshake.stop()
+
+func _on_started_shaking() -> void:
+    _is_shaking = true
+func _on_stopped_shaking() -> void:
+    _is_shaking = false
