e6c8f475e19ff0564db237fb8e9948c7209cec1d
Federico Menozzi <federicogmenozzi@gmail.com>
2020-02-16 15:10:03 -0800

    Add direction to worker drone.

--- actors/enemies/worker_drone/worker_drone.gd
+++ actors/enemies/worker_drone/worker_drone.gd
@@ -1,14 +1,23 @@
 extends KinematicBody2D
 
+export(Util.Direction) var direction := Util.Direction.RIGHT
+
 onready var _health: Health = $Health
 onready var _flash_manager: Node = $FlashManager
+onready var _sprite: Sprite = $Sprite
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
     _animation_player.play('idle')
 
+    set_direction(direction)
+
     _health.connect('health_changed', self, '_on_health_changed')
     _health.connect('died', self, '_on_died')
+
+func set_direction(new_direction: int) -> void:
+    direction = new_direction
+    _sprite.flip_h = (new_direction == Util.Direction.LEFT)
 
 func take_hit(damage: int, player: Player) -> void:
     _health.take_damage(damage)
