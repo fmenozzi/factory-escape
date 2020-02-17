8c2a6f4d2037c1797ed3412adcca395fbb6b8bba
Federico Menozzi <federicogmenozzi@gmail.com>
2020-02-16 16:00:24 -0800

    Clean up slime-related code.

--- actors/enemies/slime/slime.gd
+++ actors/enemies/slime/slime.gd
@@ -33,6 +33,7 @@ onready var _dust_puff: Particles2D = $DustPuff
 
 onready var _health: Health = $Health
 onready var _hurtbox: Area2D = $Hurtbox
+onready var _sprite: Sprite = $Sprite
 
 onready var _edge_raycast_left: RayCast2D = $LedgeDetectorRaycasts/Left
 onready var _edge_raycast_right: RayCast2D = $LedgeDetectorRaycasts/Right
@@ -69,17 +70,7 @@ func move(velocity: Vector2, snap: Vector2 = Util.SNAP) -> void:
 
 func set_direction(new_direction: int) -> void:
     direction = new_direction
-    $Sprite.flip_h = (new_direction == Util.Direction.LEFT)
-
-func is_touching_hazard() -> bool:
-    # Since there doesn't seem to be a way for a KinematicBody2D to query the
-    # Area2Ds that overlap it, we just use the hurtbox Area2D to detect
-    # collisions with hazards like spikes, taking advantage of the fact that the
-    # collision shapes are the same.
-    for area in _hurtbox.get_overlapping_areas():
-        if Util.in_collision_layer(area, 'hazards'):
-            return true
-    return false
+    _sprite.flip_h = (new_direction == Util.Direction.LEFT)
 
 func is_off_ledge() -> bool:
     var off_left := not _edge_raycast_left.is_colliding()
--- actors/enemies/slime/states/stagger.gd
+++ actors/enemies/slime/states/stagger.gd
@@ -4,30 +4,29 @@ extends 'res://actors/enemies/state.gd'
 const STAGGER_DURATION: float = 0.1
 
 # The speed at which the slime is knocked back in pixels per second.
-const STAGGER_SPEED: float = 10.0 * Util.TILE_SIZE
+const STAGGER_SPEED: float = 2.5 * Util.TILE_SIZE
 
 onready var _stagger_duration_timer: Timer = $StaggerDurationTimer
 
-var _velocity := Vector2.ZERO
 var _direction_from_hit: int = Util.Direction.NONE
 
 func _ready() -> void:
     _stagger_duration_timer.wait_time = STAGGER_DURATION
     _stagger_duration_timer.one_shot = true
 
-func enter(slime, previous_state_dict: Dictionary) -> void:
+func enter(slime: Slime, previous_state_dict: Dictionary) -> void:
     _stagger_duration_timer.start()
 
     _direction_from_hit = previous_state_dict['direction_from_hit']
     assert(_direction_from_hit != null)
 
-func exit(slime) -> void:
+func exit(slime: Slime) -> void:
     pass
 
-func update(slime, delta: float) -> Dictionary:
+func update(slime: Slime, delta: float) -> Dictionary:
     if _stagger_duration_timer.is_stopped():
         return {'new_state': Slime.State.WALK}
 
-    slime.move(Vector2(_direction_from_hit * 10.0 * slime.SPEED, 1))
+    slime.move(Vector2(_direction_from_hit * STAGGER_SPEED, 1))
 
     return {'new_state': Slime.State.NO_CHANGE}
