ccee4561b0286b28057e214b3d786dda3e73f250
Federico Menozzi <federicogmenozzi@gmail.com>
2020-03-01 13:43:45 -0800

    Cache player instance in sentry drone's idle state.

--- actors/enemies/sentry_drone/states/idle.gd
+++ actors/enemies/sentry_drone/states/idle.gd
@@ -1,14 +1,18 @@
 extends 'res://actors/enemies/state.gd'
 
+var _player: Player = null
+
 func enter(sentry_drone: SentryDrone, previous_state_dict: Dictionary) -> void:
     sentry_drone.get_animation_player().play('idle')
+
+    _player = Util.get_player()
 
 func exit(sentry_drone: SentryDrone) -> void:
     pass
 
 func update(sentry_drone: SentryDrone, delta: float) -> Dictionary:
     var distance_to_player := sentry_drone.global_position.distance_to(
-        Util.get_player().global_position)
+        _player.global_position)
     if distance_to_player <= 4.0 * Util.TILE_SIZE:
         return {'new_state': SentryDrone.State.BASH_TELEGRAPH_SHAKE}
 
