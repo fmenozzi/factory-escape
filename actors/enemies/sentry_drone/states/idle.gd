d01ae719b06fc2239af2bbab3c88a08d849f69db
Federico Menozzi <federicogmenozzi@gmail.com>
2020-03-14 10:30:03 -0700

    Use named constant for sentry drone aggro radius.

--- actors/enemies/sentry_drone/states/idle.gd
+++ actors/enemies/sentry_drone/states/idle.gd
@@ -1,5 +1,7 @@
 extends 'res://actors/enemies/state.gd'
 
+const AGGRO_RADIUS := 4.0 * Util.TILE_SIZE
+
 var _player: Player = null
 
 func enter(sentry_drone: SentryDrone, previous_state_dict: Dictionary) -> void:
@@ -13,7 +15,7 @@ func exit(sentry_drone: SentryDrone) -> void:
 func update(sentry_drone: SentryDrone, delta: float) -> Dictionary:
     var distance_to_player := sentry_drone.global_position.distance_to(
         _player.get_center())
-    if distance_to_player <= 4.0 * Util.TILE_SIZE:
+    if distance_to_player <= AGGRO_RADIUS:
         return {'new_state': SentryDrone.State.BASH_TELEGRAPH_SHAKE}
 
     return {'new_state': SentryDrone.State.NO_CHANGE}
