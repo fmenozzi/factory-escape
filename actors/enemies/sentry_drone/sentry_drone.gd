6fbb0ed7b3482a00bc7a841ccabb7bcfac1555e5
Federico Menozzi <federicogmenozzi@gmail.com>
2020-03-14 11:31:43 -0700

    Make sentry drone actually die and set health to 3.

--- actors/enemies/sentry_drone/sentry_drone.gd
+++ actors/enemies/sentry_drone/sentry_drone.gd
@@ -29,6 +29,9 @@ onready var _sprite: Sprite = $Sprite
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
+    _health.connect('health_changed', self, '_on_health_changed')
+    _health.connect('died', self, '_on_died')
+
     _current_state_enum = State.IDLE
     _current_state = STATES[_current_state_enum]
     _change_state({'new_state': _current_state_enum})
@@ -79,3 +82,11 @@ func _change_state(new_state_dict: Dictionary) -> void:
     _current_state_enum = new_state_enum
     _current_state = STATES[new_state_enum]
     _current_state.enter(self, new_state_dict)
+
+func _on_health_changed(old_health: int, new_health: int) -> void:
+    print('SENTRY DRONE HIT (new health: ', new_health, ')')
+
+# TODO: Make death nicer (animation, effects, etc.).
+func _on_died() -> void:
+    print('SENTRY DRONE DIED')
+    queue_free()
