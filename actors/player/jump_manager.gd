12ced114c51b3fb3bbec5159ee383a4f15d4d39b
Federico Menozzi <federicogmenozzi@gmail.com>
2020-03-22 14:31:18 -0700

    Store max num jumps separately in jump manager.

--- actors/player/jump_manager.gd
+++ actors/player/jump_manager.gd
@@ -1,9 +1,13 @@
 extends Node2D
 class_name JumpManager
 
-var _jumps_remaining := 2
+var _max_num_jumps := 2
+var _jumps_remaining := 0
 
 onready var _jump_buffer_raycast: RayCast2D = $JumpBufferRaycast
+
+func _ready() -> void:
+    _jumps_remaining = _max_num_jumps
 
 func can_jump() -> bool:
     return _jumps_remaining > 0
@@ -12,7 +16,7 @@ func consume_jump() -> void:
     _jumps_remaining -= 1
 
 func reset_jump() -> void:
-    _jumps_remaining = 2
+    _jumps_remaining = _max_num_jumps
 
 func get_jump_buffer_raycast() -> RayCast2D:
     return _jump_buffer_raycast
