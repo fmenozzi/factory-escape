9d07f98f19fc6b4191768d9cd92b5973adb805d6
Federico Menozzi <federicogmenozzi@gmail.com>
2020-03-28 14:54:18 -0700

    Switch to using state-based logic in jump manager.
    This should help clarify the rules surrounding when the player can jump,
    and may lead to a better jump manager API in the future.

--- actors/player/jump_manager.gd
+++ actors/player/jump_manager.gd
@@ -1,22 +1,55 @@
 extends Node2D
 class_name JumpManager
 
-var _max_num_jumps := 2
-var _jumps_remaining := 0
+enum State {
+    NOT_JUMPED,
+    JUMPED,
+    DOUBLE_JUMPED,
+}
+var _state: int = State.NOT_JUMPED
+
+var _has_double_jump: bool
 
 onready var _jump_buffer_raycast: RayCast2D = $JumpBufferRaycast
 
 func _ready() -> void:
-    _jumps_remaining = _max_num_jumps
+    _has_double_jump = true
 
 func can_jump() -> bool:
-    return _jumps_remaining > 0
+    assert(_state in [State.NOT_JUMPED, State.JUMPED, State.DOUBLE_JUMPED])
+
+    match _state:
+        State.NOT_JUMPED:
+            # If we haven't jumped, we can jump.
+            return true
+
+        State.JUMPED:
+            # If we've already jumped, we can only jump again if we have the
+            # double jump.
+            return _has_double_jump
+
+        _:
+            # If we've already double-jumped, we can't jump anymore.
+            return false
+
+func has_double_jump() -> bool:
+    return _has_double_jump
 
 func consume_jump() -> void:
-    _jumps_remaining -= 1
+    # Shouldn't be consuming jump if we've already double-jumped.
+    assert(_state in [State.NOT_JUMPED, State.JUMPED])
+
+    match _state:
+        State.NOT_JUMPED:
+            _state = State.JUMPED
+
+        State.JUMPED:
+            if _has_double_jump:
+                _state = State.DOUBLE_JUMPED
+
 
 func reset_jump() -> void:
-    _jumps_remaining = _max_num_jumps
+    _state = State.NOT_JUMPED
 
 func get_jump_buffer_raycast() -> RayCast2D:
     return _jump_buffer_raycast
