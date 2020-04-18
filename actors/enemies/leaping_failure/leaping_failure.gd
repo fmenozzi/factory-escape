4dc79899112ce801f1f54ad3c474b5925ea0a34d
Federico Menozzi <federicogmenozzi@gmail.com>
2020-04-18 14:06:14 -0700

    Add basic skeleton for leaping failure.

---
+++ actors/enemies/leaping_failure/leaping_failure.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+class_name LeapingFailure
+
+export(Util.Direction) var direction := Util.Direction.RIGHT
+
+const SPEED := 0.5 * Util.TILE_SIZE
+
+enum State {
+    NO_CHANGE,
+    WALK,
+}
+
+onready var STATES := {
+    State.WALK: $States/Walk,
+}
+
+var _current_state: Node = null
+var _current_state_enum: int = -1
+
+onready var _sprite: Sprite = $Sprite
+
+func _ready() -> void:
+    set_direction(direction)
+
+    _current_state_enum = State.WALK
+    _current_state = STATES[_current_state_enum]
+    _change_state({'new_state': _current_state_enum})
+
+func _physics_process(delta: float) -> void:
+    var new_state_dict = _current_state.update(self, delta)
+    if new_state_dict['new_state'] != State.NO_CHANGE:
+        _change_state(new_state_dict)
+
+func set_direction(new_direction: int) -> void:
+    direction = new_direction
+    _sprite.flip_h = (new_direction == Util.Direction.LEFT)
+
+func move(velocity: Vector2, snap: Vector2 = Util.SNAP) -> void:
+    .move_and_slide_with_snap(velocity, snap, Util.FLOOR_NORMAL)
+
+func _change_state(new_state_dict: Dictionary) -> void:
+    var old_state_enum := _current_state_enum
+    var new_state_enum: int = new_state_dict['new_state']
+
+    # Before passing along the new_state_dict to the new state (since we want
+    # any additional metadata keys passed too), rename the 'new_state' key to
+    # 'previous_state'.
+    new_state_dict.erase('new_state')
+    new_state_dict['previous_state'] = old_state_enum
+
+    _current_state.exit(self)
+    _current_state_enum = new_state_enum
+    _current_state = STATES[new_state_enum]
+    _current_state.enter(self, new_state_dict)
---
+++ actors/enemies/leaping_failure/states/walk.gd
@@ -0,0 +1,15 @@
+extends 'res://actors/enemies/state.gd'
+
+func enter(failure: LeapingFailure, previous_state_dict: Dictionary) -> void:
+    failure.get_node('AnimationPlayer').play('walk')
+
+func exit(failure: LeapingFailure) -> void:
+    pass
+
+func update(failure: LeapingFailure, delta: float) -> Dictionary:
+    failure.move(Vector2(failure.direction * failure.SPEED, 10))
+
+    if failure.is_on_wall():
+        failure.set_direction(-1 * failure.direction)
+
+    return {'new_state': LeapingFailure.State.NO_CHANGE}
