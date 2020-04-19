bb531bc68ce38ae4b0605c4775bd9d7ef7e2c859
Federico Menozzi <federicogmenozzi@gmail.com>
2020-04-19 13:12:33 -0700

    Make leaping failure face player when alerted.

--- actors/enemies/leaping_failure/states/alerted.gd
+++ actors/enemies/leaping_failure/states/alerted.gd
@@ -3,13 +3,16 @@ extends 'res://actors/enemies/state.gd'
 var _player: Player = null
 
 func enter(failure: LeapingFailure, previous_state_dict: Dictionary) -> void:
+    _player = Util.get_player()
+
     # Pause current animation.
     failure.get_animation_player().stop(false)
 
     # Display alerted reaction.
     failure.get_react_sprite().change_state(ReactSprite.State.ALERTED)
 
-    _player = Util.get_player()
+    # Turn to face player when alerted.
+    failure.set_direction(Util.direction(failure, _player))
 
 func exit(failure: LeapingFailure) -> void:
     # Hide reaction sprite.
