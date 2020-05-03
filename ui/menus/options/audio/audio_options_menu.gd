50636c192250a42880502ce54ed4724f90e33764
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-03 16:00:37 -0700

    Emit click sound when changing audio values.

--- ui/menus/options/audio/audio_options_menu.gd
+++ ui/menus/options/audio/audio_options_menu.gd
@@ -1,7 +1,14 @@
 extends 'res://ui/menus/menu.gd'
 
 onready var _music_slider: HSlider = $Music/Container/Slider
+onready var _effects_slider: HSlider = $Effects/Container/Slider
+onready var _ui_slider: HSlider = $UI/Container/Slider
 onready var _back_button: Button = $Back
+
+func _ready() -> void:
+    _music_slider.connect('value_changed', self, '_on_music_value_changed')
+    _effects_slider.connect('value_changed', self, '_on_effects_value_changed')
+    _ui_slider.connect('value_changed', self, '_on_ui_value_changed')
 
 func enter(pause: Pause, previous_menu: int) -> void:
     self.visible = true
@@ -21,6 +28,15 @@ func handle_input(pause: Pause, event: InputEvent) -> void:
 
     if event.is_action_pressed('ui_up') or event.is_action_pressed('ui_down'):
         emit_menu_navigation_sound()
+
+func _on_music_value_changed(new_value: float) -> void:
+    emit_menu_navigation_sound()
+
+func _on_effects_value_changed(new_value: float) -> void:
+    emit_menu_navigation_sound()
+
+func _on_ui_value_changed(new_value: float) -> void:
+    emit_menu_navigation_sound()
 
 func _on_back_pressed(pause: Pause) -> void:
     change_menu(pause.Menu.AUDIO_OPTIONS, pause.Menu.OPTIONS)
