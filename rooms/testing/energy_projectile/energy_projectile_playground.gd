db67f52b09da63dae7ad36750b3823128ec0b40b
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-09 11:36:49 -0700

    Shoot repeatedly in EnergyProjectileDemo.

--- rooms/testing/energy_projectile/energy_projectile_playground.gd
+++ rooms/testing/energy_projectile/energy_projectile_playground.gd
@@ -1,4 +1,12 @@
 extends Room
 
+onready var _projectile_spawner: Position2D = $ProjectileSpawner
+onready var _shoot_timer: Timer = $ShootTimer
+
 func _ready() -> void:
-    $ProjectileSpawner.shoot(Vector2.RIGHT)
+    _shoot_timer.wait_time = 1.0
+    _shoot_timer.connect('timeout', self, '_on_shoot_timeout')
+    _shoot_timer.start()
+
+func _on_shoot_timeout() -> void:
+    _projectile_spawner.shoot(Vector2.RIGHT)
