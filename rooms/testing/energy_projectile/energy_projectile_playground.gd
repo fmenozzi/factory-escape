4a1f36751f3b54a53d96c6fdb293f60daa71186b
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-10 11:22:19 -0700

    Add separate trail particles to energy projectile.

--- rooms/testing/energy_projectile/energy_projectile_playground.gd
+++ rooms/testing/energy_projectile/energy_projectile_playground.gd
@@ -4,9 +4,11 @@ onready var _projectile_spawner: Position2D = $ProjectileSpawner
 onready var _shoot_timer: Timer = $ShootTimer
 
 func _ready() -> void:
-    _shoot_timer.wait_time = 1.0
-    _shoot_timer.connect('timeout', self, '_on_shoot_timeout')
+    _shoot_timer.wait_time = 2.0
+    _shoot_timer.connect('timeout', self, 'shoot')
     _shoot_timer.start()
 
-func _on_shoot_timeout() -> void:
+    shoot()
+
+func shoot() -> void:
     _projectile_spawner.shoot(Vector2.RIGHT)
