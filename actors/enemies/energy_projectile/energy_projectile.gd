68004fbb2a0fcdf83d318674ba141a183062c201
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-11 17:07:36 -0700

    Add max lifetime to energy projectile.

--- actors/enemies/energy_projectile/energy_projectile.gd
+++ actors/enemies/energy_projectile/energy_projectile.gd
@@ -2,21 +2,33 @@ extends Node2D
 
 export(float) var speed_tiles_per_second := 8.0
 
+const MAX_LIFETIME := 20.0
+
 var _velocity := Vector2.ZERO
 
 onready var _hitbox: Area2D = $Hitbox
+onready var _lifetime_timer: Timer = $LifetimeTimer
 
 func _ready() -> void:
     set_physics_process(false)
 
     _hitbox.connect('body_entered', self, '_on_impact')
 
+    _lifetime_timer.one_shot = true
+    _lifetime_timer.wait_time = MAX_LIFETIME
+    _lifetime_timer.process_mode = Timer.TIMER_PROCESS_PHYSICS
+    _lifetime_timer.connect('timeout', self, '_on_lifetime_timeout')
+
 func _physics_process(delta: float) -> void:
     position += _velocity * delta
 
 func start(direction: Vector2) -> void:
     _velocity = direction.normalized() * speed_tiles_per_second * Util.TILE_SIZE
+    _lifetime_timer.start()
     set_physics_process(true)
 
 func _on_impact(body: Node) -> void:
+    queue_free()
+
+func _on_lifetime_timeout() -> void:
     queue_free()
