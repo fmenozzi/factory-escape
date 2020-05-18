b87c386f675b5bbab3bc15feff84c6a03041921b
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-18 08:56:28 -0700

    Change ranged sentry drone homing behavior.
    Rather than perfectly following the player on every frame, have the
    ranged sentry drone home in only slightly during the FOLLOW_PLAYER
    state. This uses a technique similar to the homing projectile, except
    that the homing weight is fixed in this case, as we always want the
    drone to be following the player in some way during the FOLLOW_PLAYER
    state.

--- actors/enemies/ranged_sentry_drone/states/follow_player.gd
+++ actors/enemies/ranged_sentry_drone/states/follow_player.gd
@@ -1,14 +1,19 @@
 extends 'res://actors/enemies/state.gd'
 
-var _player: Player
-
 onready var _shoot_timer: Timer = $ShootTimer
+
+var _player: Player
+var _velocity := Vector2.ZERO
 
 func _ready() -> void:
     _shoot_timer.one_shot = true
 
 func enter(sentry_drone: RangedSentryDrone, previous_state_dict: Dictionary) -> void:
     _player = Util.get_player()
+
+    # The initial velocity is toward the player's general direction.
+    _velocity = sentry_drone.global_position.direction_to(_player.get_center())
+    _velocity = _velocity.rotated(rand_range(-PI/8.0, PI/8.0))
 
     sentry_drone.get_animation_player().play('idle')
 
@@ -21,12 +26,11 @@ func exit(sentry_drone: RangedSentryDrone) -> void:
 
 func update(sentry_drone: RangedSentryDrone, delta: float) -> Dictionary:
     var aggro_manager := sentry_drone.get_aggro_manager()
-    var physics_manager := sentry_drone.get_physics_manager()
 
     # Move toward player.
-    var dir := sentry_drone.global_position.direction_to(_player.get_center())
+    _update_velocity(sentry_drone)
     sentry_drone.set_direction(Util.direction(sentry_drone, _player))
-    sentry_drone.move(dir.normalized() * physics_manager.get_movement_speed())
+    sentry_drone.move(_velocity)
 
     # Transition to unalerted state once outside of aggro radius or once the
     # player is no longer visible.
@@ -37,3 +41,16 @@ func update(sentry_drone: RangedSentryDrone, delta: float) -> Dictionary:
         return {'new_state': RangedSentryDrone.State.SHOOT}
 
     return {'new_state': RangedSentryDrone.State.NO_CHANGE}
+
+func _update_velocity(sentry_drone: RangedSentryDrone) -> void:
+    var physics_manager := sentry_drone.get_physics_manager()
+
+    # Similar to the homing projectile, home in slightly towards the player.
+    # Unlike the homing projectile, the homing weight is fixed, as we always
+    # want to be moving toward the player during this state.
+    var current_dir := _velocity.normalized()
+    var player_dir := sentry_drone.global_position.direction_to(_player.get_center()).normalized()
+    var final_dir: Vector2 = lerp(current_dir, player_dir, 0.1).normalized()
+
+    # Update the velocity to point in the new direction.
+    _velocity += (final_dir * physics_manager.get_movement_speed() - _velocity)
