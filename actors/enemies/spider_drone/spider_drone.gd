f75aae6b8890b156ba1080291bc7774372d3217f
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-19 08:24:52 -0700

    Add spider drone skeleton and SpiderDroneDemo.

---
+++ actors/enemies/spider_drone/spider_drone.gd
@@ -0,0 +1,70 @@
+extends KinematicBody2D
+class_name SpiderDrone
+
+enum State {
+    NO_CHANGE,
+    IDLE,
+}
+
+export(Util.Direction) var direction := Util.Direction.RIGHT
+
+onready var STATES := {
+    State.IDLE: $States/Idle,
+}
+
+var _current_state: Node = null
+var _current_state_enum: int = -1
+
+onready var _health: Health = $Health
+onready var _flash_manager: Node = $FlashManager
+onready var _sprite: Sprite = $Sprite
+onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+    _health.connect('health_changed', self, '_on_health_changed')
+    _health.connect('died', self, '_on_died')
+
+    set_direction(direction)
+
+    _current_state_enum = State.IDLE
+    _current_state = STATES[_current_state_enum]
+    _change_state({'new_state': _current_state_enum})
+
+func _physics_process(delta: float) -> void:
+    var new_state_dict = _current_state.update(self, delta)
+    if new_state_dict['new_state'] != State.NO_CHANGE:
+        _change_state(new_state_dict)
+
+func set_direction(new_direction: int) -> void:
+    direction = new_direction
+    _sprite.flip_h = (new_direction == Util.Direction.LEFT)
+
+func take_hit(damage: int, player: Player) -> void:
+    _health.take_damage(damage)
+    _flash_manager.start_flashing()
+
+func get_animation_player() -> AnimationPlayer:
+    return _animation_player
+
+func _change_state(new_state_dict: Dictionary) -> void:
+    var old_state_enum := _current_state_enum
+    var new_state_enum: int = new_state_dict['new_state']
+
+    # Before passing along the new_state_dict to the new state (since we want
+    # any additional metadata keys passed too), rename the 'new_state' key to
+    # 'previous_state'.
+    new_state_dict.erase('new_state')
+    new_state_dict['previous_state'] = old_state_enum
+
+    _current_state.exit(self)
+    _current_state_enum = new_state_enum
+    _current_state = STATES[new_state_enum]
+    _current_state.enter(self, new_state_dict)
+
+func _on_health_changed(old_health: int, new_health: int) -> void:
+    print('SPIDER DRONE HIT (new health: ', new_health, ')')
+
+# TODO: Make death nicer (animation, effects, etc.).
+func _on_died() -> void:
+    print('SPIDER DRONE DIED')
+    queue_free()
