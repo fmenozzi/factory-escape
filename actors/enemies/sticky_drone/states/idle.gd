a8b3f09ab8dd41cc8f73e7948a4d5ade146f8958
Federico Menozzi <federicogmenozzi@gmail.com>
2020-05-25 17:21:05 -0700

    Have sticky drone switch between IDLE and WALK.

--- actors/enemies/sticky_drone/states/idle.gd
+++ actors/enemies/sticky_drone/states/idle.gd
@@ -1,10 +1,24 @@
 extends 'res://actors/enemies/state.gd'
 
+const IDLE_DURATION: float = 2.0
+
+onready var _idle_duration_timer: Timer = $IdleDurationTimer
+
+func _ready() -> void:
+    _idle_duration_timer.one_shot = true
+    _idle_duration_timer.wait_time = IDLE_DURATION
+
 func enter(sticky_drone: StickyDrone, previous_state_dict: Dictionary) -> void:
     sticky_drone.get_animation_player().play('idle')
+
+    _idle_duration_timer.start()
 
 func exit(sticky_drone: StickyDrone) -> void:
     pass
 
 func update(sticky_drone: StickyDrone, delta: float) -> Dictionary:
+    if _idle_duration_timer.is_stopped():
+        sticky_drone.set_direction(-1 * sticky_drone.direction)
+        return {'new_state': StickyDrone.State.WALK}
+
     return {'new_state': StickyDrone.State.NO_CHANGE}
--- actors/enemies/sticky_drone/states/walk.gd
+++ actors/enemies/sticky_drone/states/walk.gd
@@ -1,12 +1,25 @@
 extends 'res://actors/enemies/state.gd'
 
+const WALK_DURATION: float = 2.0
+
+onready var _walk_duration_timer: Timer = $WalkDurationTimer
+
+func _ready() -> void:
+    _walk_duration_timer.one_shot = true
+    _walk_duration_timer.wait_time = WALK_DURATION
+
 func enter(sticky_drone: StickyDrone, previous_state_dict: Dictionary) -> void:
     sticky_drone.get_animation_player().play('walk')
+
+    _walk_duration_timer.start()
 
 func exit(sticky_drone: StickyDrone) -> void:
     pass
 
 func update(sticky_drone: StickyDrone, delta: float) -> Dictionary:
+    if _walk_duration_timer.is_stopped():
+        return {'new_state': StickyDrone.State.IDLE}
+
     var physics_manager := sticky_drone.get_physics_manager()
 
     var velocity := Vector2(
