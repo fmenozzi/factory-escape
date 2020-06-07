4c7a7ea5ad589c0519261be1c48e2a065001e2e6
Federico Menozzi <federicogmenozzi@gmail.com>
2020-06-07 10:14:53 -0700

    Flip turret head sprite properly during rotation.

--- actors/enemies/turret/turret.gd
+++ actors/enemies/turret/turret.gd
@@ -2,10 +2,12 @@ extends Node2D
 class_name Turret
 
 onready var _head: Node2D = $Head
+onready var _head_sprite: Sprite = $Head/Sprite
 onready var _projectile_spawner: ProjectileSpawner = $Head/ProjectileSpawner
 
 func rotate_head(angle: float) -> void:
-    _head.rotation += angle
+    _head.rotation = fposmod(_head.rotation + angle, 2*PI)
+    _head_sprite.flip_v = (PI/2 <= _head.rotation and _head.rotation < 3*PI/2)
 
 func shoot() -> void:
     var direction := Vector2.RIGHT.rotated(_head.rotation)
