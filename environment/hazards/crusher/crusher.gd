02f6f5150acc7155b4fdf017d468dbce0221d322
Federico Menozzi <federicogmenozzi@gmail.com>
2020-06-19 08:02:11 -0700

    Add screenshake and dust puffs on crusher impact.

--- environment/hazards/crusher/crusher.gd
+++ environment/hazards/crusher/crusher.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
+onready var _dust_puffs: Array = $CrusherHead/DustPuffs.get_children()
 
 func _ready() -> void:
     _animation_player.play('crush_loop')
+
+func _impact() -> void:
+    Screenshake.start(Screenshake.Duration.SHORT, Screenshake.Amplitude.SMALL)
+
+    for dust_puff in _dust_puffs:
+        dust_puff.emitting = true
