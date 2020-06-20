1a9b367ef444d3fcde3db54b476ccfd86b2a1a7c
Federico Menozzi <federicogmenozzi@gmail.com>
2020-06-20 11:10:41 -0700

    Add faster version of crusher.

--- environment/hazards/crusher/crusher.gd
+++ environment/hazards/crusher/crusher.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
+enum Speed {
+    SLOW,
+    FAST,
+}
+
 export(float) var initial_delay := 0.0
+export(Speed) var speed := Speed.SLOW
 
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 onready var _dust_puffs: Array = $CrusherHead/DustPuffs.get_children()
 
 func _ready() -> void:
     yield(get_tree().create_timer(initial_delay), 'timeout')
-    _animation_player.play('crush_loop')
+
+    match speed:
+        Speed.SLOW:
+            _animation_player.play('crush_loop_slow')
+        Speed.FAST:
+            _animation_player.play('crush_loop_fast')
 
 func _impact() -> void:
     if _player_is_near():
