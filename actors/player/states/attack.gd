2e0252eae680da79192c325bb57eb82ecfb251f2
Federico Menozzi <federicogmenozzi@gmail.com>
2020-06-25 08:06:02 -0700

    Allow direct transitions from ATTACK to ATTACK.

--- actors/player/states/attack.gd
+++ actors/player/states/attack.gd
@@ -1,17 +1,27 @@
 extends 'res://actors/player/states/player_state.gd'
 
+var _attack_again := false
+
 func enter(player: Player, previous_state_dict: Dictionary) -> void:
     player.start_attack(player.get_attack_manager().get_next_attack_animation())
+
+    _attack_again = false
 
 func exit(player: Player) -> void:
     player.stop_attack()
 
 func handle_input(player: Player, event: InputEvent) -> Dictionary:
+    if event.is_action_pressed('player_attack'):
+        if player.get_attack_manager().can_attack():
+            _attack_again = true
+
     return {'new_state': Player.State.NO_CHANGE}
 
 func update(player: Player, delta: float) -> Dictionary:
     if not player.get_animation_player().is_playing():
-        if player.is_in_air():
+        if _attack_again:
+            return {'new_state': Player.State.ATTACK}
+        elif player.is_in_air():
             return {'new_state': Player.State.FALL}
         else:
             return {'new_state': Player.State.IDLE}
