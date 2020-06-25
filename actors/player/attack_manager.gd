1b6e4645cc92571fa52ca6082c807e93c15b7ade
Federico Menozzi <federicogmenozzi@gmail.com>
2020-06-25 07:28:28 -0700

    Add third attack to combo and combo cooldown.
    For now, the third attack is identical to the first, though this may
    change in the future. The entire three-hit combo sequence now has a
    slight cooldown before attacking becomes possible again.

--- actors/player/attack_manager.gd
+++ actors/player/attack_manager.gd
@@ -4,6 +4,13 @@ class_name AttackManager
 onready var _attack_combo_timer: Timer = $AttackComboTimer
 onready var _attack_cooldown_timer: Timer = $AttackCooldownTimer
 
+enum CurrentAttack {
+    ATTACK_1,
+    ATTACK_2,
+    ATTACK_3,
+}
+var _current_attack: int = CurrentAttack.ATTACK_1
+
 func _ready() -> void:
     _attack_combo_timer.one_shot = true
     _attack_combo_timer.wait_time = 0.5
@@ -12,15 +19,19 @@ func _ready() -> void:
     _attack_cooldown_timer.wait_time = 0.5
 
 func get_next_attack_animation() -> String:
-    # Attack using appropriate animation, depending on how much time has passed
-    # since the last attack.
     if _attack_combo_timer.is_stopped():
         _attack_combo_timer.start()
+        _current_attack = CurrentAttack.ATTACK_1
         return 'attack_1'
+
+    if _current_attack == CurrentAttack.ATTACK_1:
+        _attack_combo_timer.start()
+        _current_attack = CurrentAttack.ATTACK_2
+        return 'attack_2'
     else:
         _attack_combo_timer.stop()
         _attack_cooldown_timer.start()
-        return 'attack_2'
+        return 'attack_3'
 
 func can_attack() -> bool:
     return _attack_cooldown_timer.is_stopped()
