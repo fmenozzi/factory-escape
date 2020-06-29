b4f65ae2411bdd697c7638d92759540d9e8f2f61
Federico Menozzi <federicogmenozzi@gmail.com>
2020-06-29 08:21:20 -0700

    Add pushback to ranged sentry drone.

--- actors/enemies/ranged_sentry_drone/ranged_sentry_drone.gd
+++ actors/enemies/ranged_sentry_drone/ranged_sentry_drone.gd
@@ -31,6 +31,7 @@ onready var _health: Health = $Health
 onready var _flash_manager: Node = $FlashManager
 onready var _physics_manager: PhysicsManager = $PhysicsManager
 onready var _aggro_manager: AggroManager = $AggroManager
+onready var _pushback_manager: PushbackManager = $PushbackManager
 onready var _react_sprite: ReactSprite = $ReactSprite
 onready var _sprite: Sprite = $Sprite
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
@@ -49,6 +50,9 @@ func _ready() -> void:
     _change_state({'new_state': _current_state_enum})
 
 func _physics_process(delta: float) -> void:
+    if _pushback_manager.is_being_pushed_back():
+        move(_pushback_manager.get_pushback_velocity())
+
     var new_state_dict = _current_state.update(self, delta)
     if new_state_dict['new_state'] != State.NO_CHANGE:
         _change_state(new_state_dict)
@@ -63,6 +67,9 @@ func set_direction(new_direction: int) -> void:
 func take_hit(damage: int, player: Player) -> void:
     _health.take_damage(damage)
     _flash_manager.start_flashing()
+
+    _pushback_manager.start_pushback(
+        player.get_center().direction_to(global_position))
 
 func get_physics_manager() -> PhysicsManager:
     return _physics_manager
