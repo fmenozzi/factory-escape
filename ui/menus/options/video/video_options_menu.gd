75370ae33c5284138ad46e839133ef6e636a02e1
Federico Menozzi <federicogmenozzi@gmail.com>
2020-07-14 07:52:11 -0700

    Save and load video options.

--- ui/menus/options/video/video_options_menu.gd
+++ ui/menus/options/video/video_options_menu.gd
@@ -1,5 +1,7 @@
 extends 'res://ui/menus/menu.gd'
 
+const SECTION := 'video'
+
 onready var _vsync: CheckBox = $VSync
 onready var _fullscreen: CheckBox = $Fullscreen
 onready var _back_button: Button = $Back
@@ -9,6 +11,10 @@ func _ready() -> void:
     _fullscreen.connect('pressed', self, '_on_fullscreen_pressed')
 
     _back_button.connect('pressed', self, '_on_back_pressed')
+
+    var default_options_data := get_options_data()
+    _vsync.pressed = default_options_data[1]['vsync']
+    _fullscreen.pressed = default_options_data[1]['fullscreen']
 
 func enter(previous_menu: int) -> void:
     self.visible = true
@@ -28,13 +34,30 @@ func handle_input(event: InputEvent) -> void:
     if event.is_action_pressed('ui_up') or event.is_action_pressed('ui_down'):
         emit_menu_navigation_sound()
 
+func get_options_data() -> Array:
+    return [SECTION, {
+        'vsync': OS.vsync_enabled,
+        'fullscreen': OS.window_fullscreen,
+    }]
+
+func load_options_data(config: ConfigFile) -> void:
+    if config.has_section_key(SECTION, 'vsync'):
+        var vsync: bool = config.get_value(SECTION, 'vsync')
+        OS.set_use_vsync(vsync)
+        _vsync.pressed = vsync
+
+    if config.has_section_key(SECTION, 'fullscreen'):
+        var fullscreen: bool = config.get_value(SECTION, 'fullscreen')
+        OS.window_fullscreen = fullscreen
+        _fullscreen.pressed = fullscreen
+
 func _on_vsync_pressed() -> void:
-    # TODO: Save this somewhere persistent as well.
     OS.set_use_vsync(_vsync.is_pressed())
+    Options.save_options()
 
 func _on_fullscreen_pressed() -> void:
-    # TODO: Save this somewhere persistent as well.
     OS.set_window_fullscreen(_fullscreen.is_pressed())
+    Options.save_options()
 
 func _on_back_pressed() -> void:
     go_to_previous_menu()
