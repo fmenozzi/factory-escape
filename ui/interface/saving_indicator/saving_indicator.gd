efd1d6dd594faee448152973d50118253e12c3b3
Federico Menozzi <federicogmenozzi@gmail.com>
2020-07-23 08:31:57 -0700

    Use sprite animation for saving indicator.
    This both matches the existing pixel art aesthetic better and is also
    vastly simpler than the previous polygon-based solution.

--- ui/interface/saving_indicator/saving_indicator.gd
+++ ui/interface/saving_indicator/saving_indicator.gd
@@ -1,60 +1,14 @@
 extends Control
 
-signal one_spin_completed
-
-export(float) var duration := 0.25
-
-onready var _upper_left: Polygon2D = $UpperLeft
-onready var _upper_right: Polygon2D = $UpperRight
-onready var _lower_right: Polygon2D = $LowerRight
-onready var _lower_left: Polygon2D = $LowerLeft
-
-onready var _tween: Tween = $AlphaTween
+onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
     hide()
-    _start_spinning()
 
 func show() -> void:
+    _animation_player.play('spin')
     self.visible = true
 
 func hide() -> void:
+    _animation_player.stop(true)
     self.visible = false
-
-func _start_spinning() -> void:
-    while true:
-        _spin_once()
-        yield(self, 'one_spin_completed')
-
-func _spin_once() -> void:
-    _tween.remove_all()
-
-    _setup_tween_fade_in(_lower_left)
-    _setup_tween_fade_out(_upper_left)
-    _tween.start()
-    yield(_tween, 'tween_all_completed')
-
-    _setup_tween_fade_in(_upper_left)
-    _setup_tween_fade_out(_upper_right)
-    _tween.start()
-    yield(_tween, 'tween_all_completed')
-
-    _setup_tween_fade_in(_upper_right)
-    _setup_tween_fade_out(_lower_right)
-    _tween.start()
-    yield(_tween, 'tween_all_completed')
-
-    _setup_tween_fade_in(_lower_right)
-    _setup_tween_fade_out(_lower_left)
-    _tween.start()
-    yield(_tween, 'tween_all_completed')
-
-    emit_signal('one_spin_completed')
-
-func _setup_tween_fade_in(tri: Polygon2D) -> void:
-    _tween.interpolate_property(
-        tri, 'color:a', 0.0, 1.0, duration, Tween.TRANS_LINEAR, Tween.EASE_IN)
-
-func _setup_tween_fade_out(tri: Polygon2D) -> void:
-    _tween.interpolate_property(
-        tri, 'color:a', 1.0, 0.0, duration, Tween.TRANS_LINEAR, Tween.EASE_IN)
