9e2bd13903a91a15809b6bca63df6bffbd5826ee
Federico Menozzi <federicogmenozzi@gmail.com>
2020-07-25 14:56:11 -0700

    Use EMPTY for slot text if save data is missing.

--- ui/menus/save_slots/save_slot_container.gd
+++ ui/menus/save_slots/save_slot_container.gd
@@ -11,7 +11,10 @@ onready var _delete: Button = $Delete
 func _ready() -> void:
     assert(save_slot in [1, 2, 3])
 
-    _slot.text = 'Slot %d' % save_slot
+    if SaveAndLoad.has_save_data(save_slot):
+        _slot.text = 'Slot %d' % save_slot
+    else:
+        _slot.text = 'EMPTY'
 
     _slot.connect('pressed', self, '_on_slot_button_pressed')
     _delete.connect('pressed', self, '_on_delete_button_pressed')
