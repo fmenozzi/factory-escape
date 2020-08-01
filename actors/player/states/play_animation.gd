e8ad45374ec153e34077057d2bbb06c6e8792aae
Federico Menozzi <federicogmenozzi@gmail.com>
2020-08-01 09:59:12 -0700

    Use shared play_animation state for sequences.

--- actors/player/states/play_animation.gd
+++ actors/player/states/play_animation.gd
@@ -1,7 +1,18 @@
 extends 'res://actors/player/states/player_state.gd'
 
+export(String) var animation_name := ''
+
+func _get_configuration_warning() -> String:
+    if animation_name == '':
+        return 'Animation name must not be empty!'
+
+    return ''
+
+func _ready() -> void:
+    assert(animation_name != '')
+
 func enter(player: Player, previous_state_dict: Dictionary) -> void:
-    player.get_animation_player().play('light_lamp')
+    player.get_animation_player().play(animation_name)
 
 func exit(player: Player) -> void:
     pass
