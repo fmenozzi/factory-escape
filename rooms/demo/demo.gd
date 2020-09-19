83b6e5d668616cdec0492889b64f89c7cf50352a
Federico Menozzi <federicogmenozzi@gmail.com>
2020-09-19 11:38:02 -0700

    Create ContainmentArena room for demo.

--- rooms/demo/demo.gd
+++ rooms/demo/demo.gd
@@ -36,6 +36,7 @@ const ABILITY_ROOMS := {
         [preload('res://rooms/demo/rooms/double_jump/ContainmentControl.tscn'), Vector2(7040, 900)],
         [preload('res://rooms/demo/rooms/double_jump/ContainmentShaftConnector.tscn'), Vector2(7680, 1080)],
         [preload('res://rooms/demo/rooms/double_jump/ContainmentShaft.tscn'), Vector2(8000, 720)],
+        [preload('res://rooms/demo/rooms/double_jump/ContainmentArena.tscn'), Vector2(8320, 720)],
     ],
 
     DemoAbility.Ability.GRAPPLE: [
---
+++ rooms/demo/rooms/double_jump/containment_arena.gd
@@ -0,0 +1,181 @@
+extends Room
+
+const SAVE_KEY := 'containment_arena'
+
+enum RoomState {
+    PRE_FIGHT,
+    WAVE_1,
+    WAVE_2,
+    WAVE_3,
+    POST_FIGHT,
+}
+
+var _current_room_state: int = RoomState.PRE_FIGHT
+var _current_wave_enemy_count := 0
+var _player_camera: Camera2D = null
+
+onready var _enemies_node: Node2D = $Enemies
+onready var _closing_door_left: StaticBody2D = $ClosingDoorLeft
+onready var _closing_door_right: StaticBody2D = $ClosingDoorRight
+onready var _arena_camera_anchor: Position2D = $CameraAnchors/Right
+onready var _door_trigger: Area2D = $ClosingDoorTrigger
+
+func _ready() -> void:
+    _door_trigger.connect('body_entered', self, '_start_arena')
+
+    set_process(false)
+
+func _process(delta: float) -> void:
+    match _current_room_state:
+        RoomState.PRE_FIGHT:
+            # Pin the camera to the center of the room.
+            _player_camera.detach_and_move_to_global(
+                self.to_global(_arena_camera_anchor.position))
+
+            # Close both doors.
+            _closing_door_left.close()
+            _closing_door_right.close()
+
+            # Spawn wave 1 enemies.
+            var leaping_failure_left := Preloads.LeapingFailure.instance()
+            var leaping_failure_far_left := Preloads.LeapingFailure.instance()
+            var leaping_failure_right := Preloads.LeapingFailure.instance()
+            leaping_failure_right.initial_direction = Util.Direction.LEFT
+
+            _spawn_enemy_at(leaping_failure_left, Vector2(408, 160))
+            _spawn_enemy_at(leaping_failure_far_left, Vector2(368, 160))
+            _spawn_enemy_at(leaping_failure_right, Vector2(552, 160))
+
+            _current_room_state = RoomState.WAVE_1
+
+        RoomState.WAVE_1:
+            if _current_wave_enemy_count == 0:
+                # Spawn wave 2 enemies.
+                var sentry_drone_left := Preloads.SentryDrone.instance()
+                var sentry_drone_right := Preloads.SentryDrone.instance()
+                sentry_drone_right.initial_direction = Util.Direction.LEFT
+                var sticky_drone := Preloads.StickyDrone.instance()
+                sticky_drone.floor_normal = StickyDrone.FloorNormal.DOWN
+
+                _spawn_enemy_at(sentry_drone_left, Vector2(408, 112))
+                _spawn_enemy_at(sentry_drone_right, Vector2(552, 112))
+                _spawn_enemy_at(sticky_drone, Vector2(480, 100))
+
+                _current_room_state = RoomState.WAVE_2
+
+        RoomState.WAVE_2:
+            if _current_wave_enemy_count == 0:
+                # Spawn wave 3 enemies.
+                var sticky_drone_left := Preloads.StickyDrone.instance()
+                sticky_drone_left.floor_normal = StickyDrone.FloorNormal.RIGHT
+                var sticky_drone_right := Preloads.StickyDrone.instance()
+                sticky_drone_right.floor_normal = StickyDrone.FloorNormal.LEFT
+                var sticky_drone_floor_left := Preloads.StickyDrone.instance()
+                sticky_drone_floor_left.floor_normal = StickyDrone.FloorNormal.UP
+                var sticky_drone_floor_right := Preloads.StickyDrone.instance()
+                sticky_drone_floor_right.floor_normal = StickyDrone.FloorNormal.UP
+
+                _spawn_enemy_at(sticky_drone_left, Vector2(336, 96))
+                _spawn_enemy_at(sticky_drone_right, Vector2(624, 96))
+                _spawn_enemy_at(sticky_drone_floor_left, Vector2(400, 160))
+                _spawn_enemy_at(sticky_drone_floor_right, Vector2(560, 160))
+
+                _current_room_state = RoomState.WAVE_3
+
+        RoomState.WAVE_3:
+            if _current_wave_enemy_count == 0:
+                # Re-attach camera to player.
+                _player_camera.reattach()
+
+                # Open both doors.
+                _closing_door_left.open()
+                _closing_door_right.open()
+
+                _current_room_state = RoomState.POST_FIGHT
+
+        RoomState.POST_FIGHT:
+            set_process(false)
+
+func reset() -> void:
+    set_process(false)
+
+    _closing_door_left.open()
+    _closing_door_right.open()
+
+    _current_wave_enemy_count = 0
+
+    # Despawn all enemies in case the player dies during the arena fight.
+    for enemy in _enemies_node.get_children():
+        enemy.queue_free()
+
+    # Unless the player has already completed the arena, reset to PRE_FIGHT
+    # state on lamp rest (e.g. if the player dies in the middle of the fight).
+    if _current_room_state != RoomState.POST_FIGHT:
+        _current_room_state = RoomState.PRE_FIGHT
+
+        # In case the player died while the camera was detached in the middle
+        # of the arena fight, reattach the camera without tweening it (i.e. set
+        # the camera's position to (0, 0) immediately). If the player dies
+        # before reaching the arena, the player camera will be null.
+        if _player_camera != null:
+            _player_camera.reattach(false)
+
+func get_save_data() -> Array:
+    return [SAVE_KEY, {
+        'current_room_state': _current_room_state,
+    }]
+
+func load_save_data(all_save_data: Dictionary) -> void:
+    if not SAVE_KEY in all_save_data:
+        return
+
+    var arena_save_data: Dictionary = all_save_data[SAVE_KEY]
+    assert('current_room_state' in arena_save_data)
+
+    # If we haven't already completed the arena, make sure room state gets set
+    # to PRE_FIGHT (i.e. in case the player quits out during one of the waves).
+    _current_room_state = arena_save_data['current_room_state']
+    if _current_room_state != RoomState.POST_FIGHT:
+        _current_room_state = RoomState.PRE_FIGHT
+
+    # If we've already completed the arena, disconnect the door trigger signal.
+    if _current_room_state == RoomState.POST_FIGHT:
+        _door_trigger.disconnect('body_entered', self, '_start_arena')
+
+func _start_arena(player: Player) -> void:
+    if not player:
+        return
+
+    if _current_room_state != RoomState.PRE_FIGHT:
+        return
+
+    _player_camera = player.get_camera()
+    assert(_player_camera != null)
+
+    set_process(true)
+
+func _spawn_enemy_at(enemy: Node2D, spawn_point_local: Vector2) -> void:
+    enemy.get_node('Health').connect('died', self, '_on_enemy_death', [enemy])
+
+    # Tween transparency so that enemies fade in as they spawn.
+    var prop := 'modulate'
+    var old := Color(1, 1, 1, 0) # Transparent
+    var new := Color(1, 1, 1, 1) # Opaque
+    var duration := 0.5
+    var trans := Tween.TRANS_LINEAR
+    var easing := Tween.EASE_IN
+
+    var alpha_tween := Tween.new()
+    alpha_tween.interpolate_property(
+        enemy, prop, old, new, duration, trans, easing)
+    enemy.add_child(alpha_tween)
+
+    _current_wave_enemy_count += 1
+
+    _enemies_node.add_child(enemy)
+    enemy.position = spawn_point_local
+    alpha_tween.start()
+
+func _on_enemy_death(enemy: KinematicBody2D) -> void:
+    print(enemy.name, ' died')
+    _current_wave_enemy_count -= 1
