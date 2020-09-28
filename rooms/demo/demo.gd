7f0f944eed7ccc92bcc32aeb9b57088bea8a1c0b
Federico Menozzi <federicogmenozzi@gmail.com>
2020-09-28 05:44:44 -0700

    Create UShaftConnector room for demo.

--- rooms/demo/demo.gd
+++ rooms/demo/demo.gd
@@ -46,6 +46,7 @@ const ABILITY_ROOMS := {
         [preload('res://rooms/demo/rooms/grapple/SpikeTrap.tscn'), Vector2(4480, 900)],
         [preload('res://rooms/demo/rooms/grapple/MaintenanceTunnel.tscn'), Vector2(4800, 900)],
         [preload('res://rooms/demo/rooms/grapple/UShaft.tscn'), Vector2(5440, 900)],
+        [preload('res://rooms/demo/rooms/grapple/UShaftConnector.tscn'), Vector2(5760, 900)],
     ],
 
     DemoAbility.Ability.WALL_JUMP: [
