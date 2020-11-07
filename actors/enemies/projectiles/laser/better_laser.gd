7d908bbe03362871302e86d6c0c3402281853486
Federico Menozzi <federicogmenozzi@gmail.com>
2020-11-07 11:39:19 -0800

    Add hitbox to new laser.

--- actors/enemies/projectiles/laser/better_laser.gd
+++ actors/enemies/projectiles/laser/better_laser.gd
@@ -3,9 +3,12 @@ extends Node2D
 # The maximum length of the laser beam.
 const MAX_LENGTH: float = 100.0 * Util.TILE_SIZE
 
+# The maximum width of the laser beam.
+const MAX_WIDTH: float = 4.0
+
 # The amount of time the laser spends winding up to its full width or down to
 # zero width during startup/wind down.
-const WIND_UP_DOWN_DURATION: float = 0.50
+const WIND_DOWN_DURATION: float = 0.25
 
 # The amount of time the laser spends telegraphing the subsequent shot, during
 # which the player cannot be harmed.
@@ -15,20 +18,28 @@ const TELEGRAPH_DURATION: float = 1.0
 # and can harm the player.
 const SHOT_DURATION: float = 1.0
 
-var _is_shooting := false
-var _original_width := 0.0
+enum State {
+    INACTIVE,
+    TELEGRAPH,
+    SHOOT,
+}
+
+var _current_state: int = State.INACTIVE
 
 onready var _raycast: RayCast2D = $RayCast2D
 onready var _line: Line2D = $Line2D
 onready var _tween: Tween = $WidthTween
+onready var _hitbox_collision_shape: CollisionShape2D = $Hitbox/CollisionShape2D
 
 func _ready() -> void:
     _raycast.cast_to = Vector2(MAX_LENGTH, 0)
 
+    _line.width = MAX_WIDTH
     _line.add_point(Vector2.ZERO)
     _line.add_point(_raycast.cast_to)
 
-    _original_width = _line.width
+    _hitbox_collision_shape.shape = RectangleShape2D.new()
+    _hitbox_collision_shape.shape.extents = Vector2.ZERO
 
     set_physics_process(false)
     hide()
@@ -37,10 +48,8 @@ func _physics_process(delta: float) -> void:
     _cast_laser_beam()
 
 func shoot() -> void:
-    if _is_shooting:
+    if _current_state != State.INACTIVE:
         return
-
-    _is_shooting = true
 
     # Activate laser.
     show()
@@ -54,15 +63,16 @@ func shoot() -> void:
     _start_laser_shot()
     yield(_tween, 'tween_all_completed')
 
-    # Wind down animation.
-    _animate_beam_width(_line.width, 0, WIND_UP_DOWN_DURATION)
+    # Wind down animation. Hitbox is disabled during wind down.
+    _hitbox_collision_shape.set_deferred('disabled', true)
+    _animate_beam_width(_line.width, 0, WIND_DOWN_DURATION)
     yield(_tween, 'tween_all_completed')
 
     # Deactivate laser.
     set_physics_process(false)
     hide()
 
-    _is_shooting = false
+    _current_state = State.INACTIVE
 
 func _cast_laser_beam() -> void:
     # Get the local coordinates of the point where the laser actually makes
@@ -71,6 +81,13 @@ func _cast_laser_beam() -> void:
 
     # Update the end of the line representing the beam.
     _line.points[1] = collision_point_local
+
+    if _current_state == State.SHOOT:
+        # Update collision shape.
+        _hitbox_collision_shape.shape.extents = Vector2(
+            collision_point_local.length() / 2.0, (MAX_WIDTH / 2.0) - 1)
+        _hitbox_collision_shape.rotation = collision_point_local.angle()
+        _hitbox_collision_shape.position = collision_point_local / 2.0
 
 func _get_collision_point_local() -> Vector2:
     _raycast.force_raycast_update()
@@ -89,11 +106,17 @@ func _start_telegraph() -> void:
     var telegraph_width := 1.5
     var num_wobbles := 6
 
+    _current_state = State.TELEGRAPH
+    _hitbox_collision_shape.set_deferred('disabled', true)
+
     _wobble(telegraph_width, num_wobbles)
 
 func _start_laser_shot() -> void:
-    var shot_width := 4
+    var shot_width := MAX_WIDTH
     var num_wobbles := 8
+
+    _current_state = State.SHOOT
+    _hitbox_collision_shape.set_deferred('disabled', false)
 
     _wobble(shot_width, num_wobbles)
 
