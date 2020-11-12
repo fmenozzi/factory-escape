ec8470c5f774a0ec604a70b91dc7c703c42d0b1f
Federico Menozzi <federicogmenozzi@gmail.com>
2020-11-12 05:13:59 -0800

    Add max bash miss distance to SentryDrone.
    Once the drone's bash misses the player, it will now travel a max
    distance before stopping, instead of continuing until collision like
    before.

--- actors/enemies/sentry_drone/states/bash.gd
+++ actors/enemies/sentry_drone/states/bash.gd
@@ -1,11 +1,20 @@
 extends 'res://actors/enemies/enemy_state.gd'
 
+# The maximum distance the sentry drone will continue to travel once it misses
+# the player.
+const MAX_BASH_MISS_DISTANCE: float = 4.0 * Util.TILE_SIZE
+
 var _direction_to_player: Vector2 = Vector2.ZERO
+var _player: Player
+var _bash_miss_distance_travelled := 0.0
 
 func enter(sentry_drone: SentryDrone, previous_state_dict: Dictionary) -> void:
     assert('direction_to_player' in previous_state_dict)
     _direction_to_player = previous_state_dict['direction_to_player']
     assert(_direction_to_player != null)
+
+    _player = Util.get_player()
+    _bash_miss_distance_travelled = 0.0
 
 func exit(sentry_drone: SentryDrone) -> void:
     pass
@@ -16,6 +25,11 @@ func update(sentry_drone: SentryDrone, delta: float) -> Dictionary:
 
     sentry_drone.move(_direction_to_player * physics_manager.get_bash_speed())
 
+    if _drone_missed_player(sentry_drone):
+        _bash_miss_distance_travelled += bash_speed * delta
+        if _bash_miss_distance_travelled > MAX_BASH_MISS_DISTANCE:
+            return {'new_state': SentryDrone.State.NEXT_STATE_IN_SEQUENCE}
+
     if sentry_drone.is_colliding():
         Screenshake.start(
             Screenshake.Duration.SHORT, Screenshake.Amplitude.SMALL)
@@ -25,3 +39,9 @@ func update(sentry_drone: SentryDrone, delta: float) -> Dictionary:
         return {'new_state': SentryDrone.State.NEXT_STATE_IN_SEQUENCE}
 
     return {'new_state': SentryDrone.State.NO_CHANGE}
+
+func _drone_missed_player(sentry_drone: SentryDrone) -> bool:
+    var original_direction := _direction_to_player
+    var current_direction := sentry_drone.global_position.direction_to(_player.get_center())
+
+    return original_direction.dot(current_direction) < 0
