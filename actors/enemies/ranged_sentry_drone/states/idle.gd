fe1bfdfb51f0d22a7f24ef97d1f21da047b5a386
Federico Menozzi <federicogmenozzi@gmail.com>
2020-11-13 05:11:31 -0800

    Have RangedSentryDrone turn around while idling.
    Instead of flying to a random point, have it simply turn around like the
    regular sentry drone.

--- actors/enemies/ranged_sentry_drone/states/idle.gd
+++ actors/enemies/ranged_sentry_drone/states/idle.gd
@@ -1,18 +1,24 @@
 extends 'res://actors/enemies/enemy_state.gd'
 
-onready var _idle_duration_timer: Timer = $IdleDurationTimer
+# The minimum number of seconds the drone waits while idling before turning
+# around. A small random value will be added to this to create the final timer
+# duration in order to avoid having all the drones turn around at the same time.
+const MIN_TURN_AROUND_PERIOD: float = 2.0
+
+onready var _timer: Timer = $TurnAroundDurationTimer
 
 func _ready() -> void:
-    _idle_duration_timer.one_shot = true
+    _timer.one_shot = false
+    _timer.wait_time = MIN_TURN_AROUND_PERIOD + rand_range(0.0, 1.0)
 
 func enter(sentry_drone: RangedSentryDrone, previous_state_dict: Dictionary) -> void:
     sentry_drone.get_animation_player().play('idle')
 
-    _idle_duration_timer.wait_time = rand_range(0.5, 2.0)
-    _idle_duration_timer.start()
+    _timer.connect('timeout', self, '_on_turn_around_timeout', [sentry_drone])
+    _timer.start()
 
 func exit(sentry_drone: RangedSentryDrone) -> void:
-    pass
+    _timer.stop()
 
 func update(sentry_drone: RangedSentryDrone, delta: float) -> Dictionary:
     var aggro_manager := sentry_drone.get_aggro_manager()
@@ -20,22 +26,7 @@ func update(sentry_drone: RangedSentryDrone, delta: float) -> Dictionary:
     if aggro_manager.in_aggro_range() and aggro_manager.can_see_player():
         return {'new_state': RangedSentryDrone.State.ALERTED}
 
-    if _idle_duration_timer.is_stopped():
-        # Once we finish idling, pick a random point within the room to fly to.
-        return {
-            'new_state': RangedSentryDrone.State.FLY_TO_POINT,
-            'fly_to_point': _get_next_fly_to_point(sentry_drone),
-        }
-
     return {'new_state': RangedSentryDrone.State.NO_CHANGE}
 
-func _get_next_fly_to_point(sentry_drone: RangedSentryDrone) -> Vector2:
-    var room: Room = sentry_drone.get_parent().get_parent()
-    assert(room != null)
-
-    var room_dims := room.get_room_dimensions()
-    var global_room_pos := room.to_global(room.position)
-
-    return Vector2(
-        rand_range(global_room_pos.x, global_room_pos.x + room_dims.x),
-        rand_range(global_room_pos.y, global_room_pos.y + room_dims.y))
+func _on_turn_around_timeout(sentry_drone: RangedSentryDrone) -> void:
+    sentry_drone.set_direction(-1 * sentry_drone.direction)
