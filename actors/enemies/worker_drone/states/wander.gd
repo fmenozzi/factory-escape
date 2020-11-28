8366c902322f6b554acd256201f8acb0c7262eda
Federico Menozzi <federicogmenozzi@gmail.com>
2020-11-28 08:19:05 -0800

    Change worker drone wander behavior.
    Rather than constantly slightly changing direction, have the drone
    continue in a straight line until hitting an obstacle, after which it
    simply bounces away.
    
    This looks better in viewport mode than the previous behavior, since
    there's less precise movement (which generally looks worse with fewer
    pixels to move around in).

--- actors/enemies/worker_drone/states/wander.gd
+++ actors/enemies/worker_drone/states/wander.gd
@@ -1,33 +1,26 @@
 extends 'res://actors/enemies/enemy_state.gd'
 
-const ANGLE_MEAN := 0.0
-const ANGLE_STD := deg2rad(3)
-
 var _rng := RandomNumberGenerator.new()
 var _velocity := Vector2.ZERO
 
 func _ready() -> void:
     _rng.randomize()
 
-func enter(worker_drone: WorkerDrone, previous_state_dict: Dictionary) -> void:
     # Initial direction is random.
-    var speed = worker_drone.get_physics_manager().get_movement_speed()
-    var direction = Vector2(_rng.randf(), _rng.randf()).normalized()
-    _velocity = speed * direction
+    _velocity = Vector2.RIGHT.rotated(_rng.randf())
+
+func enter(worker_drone: WorkerDrone, previous_state_dict: Dictionary) -> void:
+    var speed := worker_drone.get_physics_manager().get_movement_speed()
+    _velocity = speed * _velocity.normalized()
 
 func exit(worker_drone: WorkerDrone) -> void:
     pass
 
 func update(worker_drone: WorkerDrone, delta: float) -> Dictionary:
-    # Deviate from the previous direction by randomly rotating the velocity
-    # vector. Sample from a normal distribution instead of a uniform one to
-    # produce less chaotic motion.
-    _velocity = _velocity.rotated(_rng.randfn(ANGLE_MEAN, ANGLE_STD))
+    # Continue moving in a straight line until we hit an obstacle, at which
+    # point we bounce away from it.
     worker_drone.move(_velocity)
-
-    # If we hit an obstacle, start moving in the opposite direction.
     if worker_drone.is_hitting_obstacle():
-        _velocity = _velocity.rotated(PI)
-        worker_drone.move(_velocity)
+        _velocity = _velocity.bounce(worker_drone.get_slide_collision(0).normal)
 
     return {'new_state': WorkerDrone.State.NO_CHANGE}
