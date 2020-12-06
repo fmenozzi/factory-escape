ff4650764f451aa3b653e75a600f768f9ac2affd
Federico Menozzi <federicogmenozzi@gmail.com>
2020-12-06 14:31:41 -0800

    Read credits data from text file.

--- ui/credits_screen/credits_screen.gd
+++ ui/credits_screen/credits_screen.gd
@@ -5,6 +5,7 @@ const SCROLL_TIMER_WAIT_TIME := 0.05
 
 onready var _timer: Timer = $ScrollSpeedTimer
 onready var _scroll_container: ScrollContainer = $ScrollContainer
+onready var _vbox_container: VBoxContainer = $ScrollContainer/VBoxContainer
 
 var _max_scroll_value := -1
 
@@ -12,6 +13,11 @@ func _ready() -> void:
     set_process_unhandled_input(false)
 
     SceneChanger.connect('scene_changed', self, '_on_scene_changed')
+
+    # Read credits data from file and add a Label to the ScrollContainer's
+    # VBoxContainer for each line in the file.
+    for label in _get_line_labels():
+        _vbox_container.add_child(label)
 
     _max_scroll_value = _scroll_container.get_v_scrollbar().max_value
 
@@ -31,6 +37,23 @@ func _unhandled_input(event: InputEvent) -> void:
 func _go_to_title_screen() -> void:
     var fade_in_delay := 2.0
     SceneChanger.change_scene_to(Preloads.TitleScreen, fade_in_delay)
+
+func _get_line_labels() -> Array:
+    var file := File.new()
+    assert(file.open('res://ui/credits_screen/data/credits.tres', File.READ) == OK)
+
+    var line_labels := []
+
+    while not file.eof_reached():
+        var label := Label.new()
+        label.align = Label.ALIGN_CENTER
+        label.text = file.get_line()
+
+        line_labels.append(label)
+
+    file.close()
+
+    return line_labels
 
 func _on_scene_changed() -> void:
     set_process_unhandled_input(true)
