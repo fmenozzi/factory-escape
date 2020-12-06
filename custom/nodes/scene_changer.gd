5b69b7a6146d2fdf1b49fc4a4db66173777d3891
Federico Menozzi <federicogmenozzi@gmail.com>
2020-12-06 13:52:08 -0800

    Add is_changing_scene() function to SceneChanger.
    In addition to querying whether the scene transition is currently taking
    place (using the newly-added function), SceneChanger will also return
    early if change_scene_to() is called during a scene transition.

--- custom/nodes/scene_changer.gd
+++ custom/nodes/scene_changer.gd
@@ -4,7 +4,14 @@ signal scene_changed
 
 onready var _screen_fadeout: Control = $ScreenFadeout
 
+var _is_changing_scene := false
+
 func change_scene_to(scene: PackedScene, fade_duration: float) -> void:
+    if _is_changing_scene:
+        return
+
+    _is_changing_scene = true
+
     _screen_fadeout.fade_to_black(fade_duration)
     yield(_screen_fadeout, 'fade_to_black_finished')
 
@@ -19,4 +26,9 @@ func change_scene_to(scene: PackedScene, fade_duration: float) -> void:
     _screen_fadeout.fade_from_black(fade_duration)
     yield(_screen_fadeout, 'fade_from_black_finished')
 
+    _is_changing_scene = false
+
     emit_signal('scene_changed')
+
+func is_changing_scene() -> bool:
+    return _is_changing_scene
