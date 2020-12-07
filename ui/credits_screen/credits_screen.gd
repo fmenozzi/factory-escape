fe2ca25e78a9bc906b6f4814eeadbb8a1ad9b38e
Federico Menozzi <federicogmenozzi@gmail.com>
2020-12-07 13:43:30 -0800

    Use VisibilityNotifier2D to know when credits end.
    The previous method didn't work because the max_value of the VScrollBar
    was always capped at 100 and doesn't seem to update after adding more
    elements to the VBoxContainer.

--- ui/credits_screen/credits_screen.gd
+++ ui/credits_screen/credits_screen.gd
@@ -7,8 +7,6 @@ onready var _timer: Timer = $ScrollSpeedTimer
 onready var _scroll_container: ScrollContainer = $ScrollContainer
 onready var _vbox_container: VBoxContainer = $ScrollContainer/VBoxContainer
 
-var _max_scroll_value := -1
-
 func _ready() -> void:
     set_process_unhandled_input(false)
 
@@ -18,8 +16,7 @@ func _ready() -> void:
     # VBoxContainer for each line in the file.
     for label in _get_line_labels():
         _vbox_container.add_child(label)
-
-    _max_scroll_value = _scroll_container.get_v_scrollbar().max_value
+    _vbox_container.add_child(_generate_end_marker())
 
     _timer.one_shot = false
     _timer.wait_time = SCROLL_TIMER_WAIT_TIME
@@ -55,16 +52,27 @@ func _get_line_labels() -> Array:
 
     return line_labels
 
+func _generate_end_marker() -> Label:
+    var label := Label.new()
+
+    var visibility_notifier := VisibilityNotifier2D.new()
+    visibility_notifier.rect = Rect2(Vector2(0, 0), Vector2(8, 8))
+    visibility_notifier.connect('screen_entered', self, '_on_credits_ended')
+
+    label.add_child(visibility_notifier)
+
+    return label
+
 func _on_scene_changed() -> void:
     set_process_unhandled_input(true)
 
 func _on_scroll_timeout() -> void:
-    _scroll_container.scroll_vertical += 1
+    _scroll_container.get_v_scrollbar().value += 1
 
-    if _scroll_container.scroll_vertical >= _max_scroll_value:
-        # Once we reach the end of the credits, disable input handling and
-        # disconnect timer callback to ensure that we only change scenes once.
-        _timer.call_deferred('disconnect', 'timeout', self, '_on_scroll_timeout')
-        set_process_unhandled_input(false)
+func _on_credits_ended() -> void:
+    # Once we reach the end of the credits, disable input handling and
+    # disconnect timer callback to ensure that we only change scenes once.
+    _timer.call_deferred('disconnect', 'timeout', self, '_on_scroll_timeout')
+    set_process_unhandled_input(false)
 
-        _go_to_title_screen()
+    _go_to_title_screen()
