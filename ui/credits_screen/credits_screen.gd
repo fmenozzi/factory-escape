ba8f05d5fd4e487a00009a8408f1dccc55519a92
Federico Menozzi <federicogmenozzi@gmail.com>
2020-12-07 14:35:31 -0800

    Add HSeparators to start and end of credits.
    The ones at the top ensure that the actual text starts in the middle of
    the screen (vertically), and the ones at the bottom ensure that the
    credit text still appears to be scrolling once we reach the end marker
    and start fading out.

--- ui/credits_screen/credits_screen.gd
+++ ui/credits_screen/credits_screen.gd
@@ -13,10 +13,17 @@ func _ready() -> void:
     SceneChanger.connect('scene_changed', self, '_on_scene_changed')
 
     # Read credits data from file and add a Label to the ScrollContainer's
-    # VBoxContainer for each line in the file.
+    # VBoxContainer for each line in the file. Pad the start and end of the
+    # credits with a series of HSeparator nodes so that the credits start lower
+    # on the screen and keep scrolling as we fade out (after reaching the end
+    # marker).
+    for hseparator in _generate_hseparators(10):
+        _vbox_container.add_child(hseparator)
     for label in _get_line_labels():
         _vbox_container.add_child(label)
     _vbox_container.add_child(_generate_end_marker())
+    for hseparator in _generate_hseparators(10):
+        _vbox_container.add_child(hseparator)
 
     _timer.one_shot = false
     _timer.wait_time = SCROLL_TIMER_WAIT_TIME
@@ -34,6 +41,12 @@ func _unhandled_input(event: InputEvent) -> void:
 func _go_to_title_screen() -> void:
     var fade_in_delay := 2.0
     SceneChanger.change_scene_to(Preloads.TitleScreen, fade_in_delay)
+
+func _generate_hseparators(num_hseparators: int) -> Array:
+    var hseparators := []
+    for i in range(num_hseparators):
+        hseparators.append(HSeparator.new())
+    return hseparators
 
 func _get_line_labels() -> Array:
     var file := File.new()
@@ -71,9 +84,8 @@ func _on_scroll_timeout() -> void:
     _scroll_container.get_v_scrollbar().value += 1
 
 func _on_credits_ended() -> void:
-    # Once we reach the end of the credits, disable input handling and
-    # disconnect timer callback to ensure that we only change scenes once.
-    _timer.call_deferred('disconnect', 'timeout', self, '_on_scroll_timeout')
+    # Once we reach the end of the credits, disable input handling to ensure
+    # that we only change scenes once.
     set_process_unhandled_input(false)
 
     _go_to_title_screen()
