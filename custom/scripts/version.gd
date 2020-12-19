0c90a05ce4ceb6eb337340e9342b516dd3fd97dc
Federico Menozzi <federicogmenozzi@gmail.com>
2020-12-19 07:58:12 -0800

    Add Version helper script for game versioning.
    This script is a series of static functions instead of an autoload, as I
    fear that I'd have to fiddle with the autoload ordering were I to make
    it an autoload (since it will end up being used in existing autoloads
    like SaveAndLoad and Options). All we need are a few static functions
    anyway, since we're just reading from ProjectSettings.

---
+++ custom/scripts/version.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Version
+
+static func major() -> int:
+    return ProjectSettings.get_setting('application/version/major')
+
+static func minor() -> int:
+    return ProjectSettings.get_setting('application/version/minor')
+
+static func patch() -> int:
+    return ProjectSettings.get_setting('application/version/patch')
+
+static func full() -> String:
+    var major := major()
+    var minor := minor()
+    var patch := patch()
+
+    assert(major >= 0 and minor >= 0 and patch >= 0, 'Invalid game version.')
+
+    return '%d.%d.%d' % [major, minor, patch]
--- ui/title_screen/version_label.gd
+++ ui/title_screen/version_label.gd
@@ -3,10 +3,4 @@ extends Control
 onready var _label: Label = $Label
 
 func _ready() -> void:
-    var major: int = ProjectSettings.get_setting('application/version/major')
-    var minor: int = ProjectSettings.get_setting('application/version/minor')
-    var patch: int = ProjectSettings.get_setting('application/version/patch')
-
-    assert(major >= 0 and minor >= 0 and patch >= 0, "Invalid game version.")
-
-    _label.text = '%d.%d.%d' % [major, minor, patch]
+    _label.text = Version.full()
