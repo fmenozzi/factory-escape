7d1bef77ae27e7857a8485857657d6f8a324ef7b
Federico Menozzi <federicogmenozzi@gmail.com>
2020-12-31 11:56:27 -0800

    Set up more robust error handling in Options.
    Future commits will turn the assert() call in _handle_error() into a
    more user-friendly solution, such as transitioning to a dedicated error
    screen.

--- custom/nodes/options.gd
+++ custom/nodes/options.gd
@@ -22,7 +22,8 @@ func save_options() -> void:
     for node in get_tree().get_nodes_in_group(GROUP):
         var options_data: Array = node.get_options_data()
 
-        assert(options_data.size() == 2)
+        if options_data.size() != 2:
+            _handle_error('Expected 2 fields in options_data, got %d' % options_data.size())
 
         var section: String = options_data[0]
         var data: Dictionary = options_data[1]
@@ -32,15 +33,20 @@ func save_options() -> void:
 
     var dir := Directory.new()
     if not dir.dir_exists(_save_directory):
-        dir.make_dir_recursive(_save_directory)
+        var error := dir.make_dir_recursive(_save_directory)
+        if error != OK:
+            _handle_error('Could not create save directory %s' % _save_directory)
 
-    var status := _config.save(_get_file_path())
-    assert(status == OK)
+    var error := _config.save(_get_file_path())
+    if error != OK:
+        _handle_error('Could not save options, error code %d' % error)
 
     emit_signal('options_saved')
 
 func load_options() -> void:
-    _load_config(_config)
+    var error := _load_config(_config)
+    if error != OK:
+        _handle_error('Could not load config, error code %d' % error)
 
     # If we don't have a valid config version, simply reset to the defaults for
     # the current game version. Otherwise, call the appropriate load function
@@ -54,7 +60,7 @@ func load_options() -> void:
                 '0.1.0':
                     node.load_options_version_0_1_0(_config)
                 _:
-                    assert(false, 'Invalid options version: ' + Version.full())
+                    _handle_error('Invalid options version: ' + Version.full())
 
     emit_signal('options_loaded')
 
@@ -83,21 +89,34 @@ func get_config() -> ConfigFile:
 func _get_file_path() -> String:
     return _save_directory + 'options.cfg'
 
-func _load_config(config_file: ConfigFile) -> void:
+func _load_config(config_file: ConfigFile) -> int:
     var path := _get_file_path()
 
-    var status := config_file.load(path)
-    if status != OK:
-        assert(status == ERR_FILE_NOT_FOUND)
+    var error := config_file.load(path)
+    if error == OK:
+        return OK
 
-        # Create new config file if it doesn't already exist.
-        var file := File.new()
-        file.open(path, File.WRITE)
-        file.close()
+    # It's ok if the file doesn't exist (which would be the case the first
+    # time the player plays the game), but any other error here is not ok.
+    if error != ERR_FILE_NOT_FOUND:
+        return error
 
-        # Add current game version to config.
-        status = config_file.load(path)
-        assert(status == OK)
-        config_file.set_value('version', 'major', Version.major())
-        config_file.set_value('version', 'minor', Version.minor())
-        config_file.set_value('version', 'patch', Version.patch())
+    # Create new config file if it doesn't already exist.
+    var file := File.new()
+    error = file.open(path, File.WRITE)
+    if error != OK:
+        return error
+    file.close()
+
+    # Add current game version to config.
+    error = config_file.load(path)
+    if error != OK:
+        return error
+    config_file.set_value('version', 'major', Version.major())
+    config_file.set_value('version', 'minor', Version.minor())
+    config_file.set_value('version', 'patch', Version.patch())
+
+    return OK
+
+func _handle_error(error_msg: String) -> void:
+    assert(false, error_msg)
