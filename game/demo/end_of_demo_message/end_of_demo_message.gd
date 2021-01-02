718da3d8d07caebfa7b0b23024143d9a1a5a78a3
Federico Menozzi <federicogmenozzi@gmail.com>
2021-01-02 12:35:31 -0800

    Use yield instead of callback in EndOfDemoMessage.

--- game/demo/end_of_demo_message/end_of_demo_message.gd
+++ game/demo/end_of_demo_message/end_of_demo_message.gd
@@ -2,8 +2,8 @@ extends Control
 
 func _ready() -> void:
     set_process_unhandled_input(false)
-
-    SceneChanger.connect('scene_changed', self, '_on_scene_changed')
+    yield(SceneChanger, 'scene_changed')
+    set_process_unhandled_input(true)
 
 func _unhandled_input(event: InputEvent) -> void:
     # Once the scene trasition completes, any key/button press advances back to
@@ -13,6 +13,3 @@ func _unhandled_input(event: InputEvent) -> void:
 
         var fade_in_delay := 2.0
         SceneChanger.change_scene_to(Preloads.TitleScreen, fade_in_delay)
-
-func _on_scene_changed() -> void:
-    set_process_unhandled_input(true)
