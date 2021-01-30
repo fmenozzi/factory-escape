b299f2e13a3ee6c92f75898ce3e2d065df625ee3
Federico Menozzi <federicogmenozzi@gmail.com>
2021-01-30 13:58:26 -0800

    Replace callback with yield in credits scene.

--- ui/credits_screen/credits_screen.gd
+++ ui/credits_screen/credits_screen.gd
@@ -8,11 +8,7 @@ onready var _scroll_container: ScrollContainer = $ScrollContainer
 onready var _vbox_container: VBoxContainer = $ScrollContainer/VBoxContainer
 
 func _ready() -> void:
-    set_process_unhandled_input(false)
-
     MouseCursor.set_mouse_mode(MouseCursor.MouseMode.HIDDEN)
-
-    SceneChanger.connect('scene_changed', self, '_on_scene_changed')
 
     # Read credits data from file and add a Label to the ScrollContainer's
     # VBoxContainer for each line in the file. Pad the start and end of the
@@ -36,6 +32,11 @@ func _ready() -> void:
     _timer.wait_time = SCROLL_TIMER_WAIT_TIME
     _timer.connect('timeout', self, '_on_scroll_timeout')
     _timer.start()
+
+    set_process_unhandled_input(false)
+    if SceneChanger.is_changing_scene():
+        yield(SceneChanger, 'scene_changed')
+    set_process_unhandled_input(true)
 
 func _unhandled_input(event: InputEvent) -> void:
     # Once the scene trasition completes, any key/button press advances back to
@@ -93,9 +94,6 @@ func _generate_end_marker() -> Label:
     label.add_child(visibility_notifier)
 
     return label
-
-func _on_scene_changed() -> void:
-    set_process_unhandled_input(true)
 
 func _on_scroll_timeout() -> void:
     _scroll_container.get_v_scrollbar().value += 1
