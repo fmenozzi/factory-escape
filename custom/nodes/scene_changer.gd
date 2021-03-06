50d48de5fd3925c2860523f4b0c8beff8655cc05
Federico Menozzi <federicogmenozzi@gmail.com>
2021-03-06 14:43:31 -0800

    Make music fade during screen fade configurable.

--- custom/nodes/scene_changer.gd
+++ custom/nodes/scene_changer.gd
@@ -6,13 +6,15 @@ onready var _screen_fadeout: Control = $ScreenFadeout
 
 var _is_changing_scene := false
 
-func change_scene_to(scene: PackedScene, fade_duration: float) -> void:
+func change_scene_to(scene: PackedScene, fade_duration: float, fade_music: bool = true) -> void:
     if _is_changing_scene:
         return
 
     _is_changing_scene = true
 
-    _screen_fadeout.fade_to_black(fade_duration)
+    var fade_delay := 0.0
+
+    _screen_fadeout.fade_to_black(fade_duration, fade_delay, fade_music)
     yield(_screen_fadeout, 'fade_to_black_finished')
 
     var status := get_tree().change_scene_to(scene)
@@ -23,7 +25,7 @@ func change_scene_to(scene: PackedScene, fade_duration: float) -> void:
     if get_tree().paused:
         get_tree().paused = false
 
-    _screen_fadeout.fade_from_black(fade_duration)
+    _screen_fadeout.fade_from_black(fade_duration, fade_delay, fade_music)
     yield(_screen_fadeout, 'fade_from_black_finished')
 
     _is_changing_scene = false
--- ui/screen_fadeout.gd
+++ ui/screen_fadeout.gd
@@ -11,32 +11,34 @@ onready var _fade_tween: Tween = $FadeTween
 
 var _global_music_slider_value: int
 
-func _ready() -> void:
-    _fade_tween.connect('tween_step', self, '_on_tween_step')
-
-func fade_to_black(duration: float, delay: float = 0.0) -> void:
-    _fade(TRANSPARENT_ALPHA, OPAQUE_ALPHA, duration, delay)
+func fade_to_black(duration: float, delay: float = 0.0, fade_music: bool = true) -> void:
+    _fade(TRANSPARENT_ALPHA, OPAQUE_ALPHA, duration, delay, fade_music)
     yield(_fade_tween, 'tween_all_completed')
 
     emit_signal('fade_to_black_finished')
 
-func fade_from_black(duration: float, delay: float = 0.0) -> void:
-    _fade(OPAQUE_ALPHA, TRANSPARENT_ALPHA, duration, delay)
+func fade_from_black(duration: float, delay: float = 0.0, fade_music: bool = true) -> void:
+    _fade(OPAQUE_ALPHA, TRANSPARENT_ALPHA, duration, delay, fade_music)
     yield(_fade_tween, 'tween_all_completed')
 
     emit_signal('fade_from_black_finished')
 
-func _fade(old: float, new: float, duration: float, delay: float) -> void:
+func _fade(old: float, new: float, duration: float, delay: float, fade_music: bool) -> void:
     _global_music_slider_value = Options.get_config().get_value('audio', 'music')
 
-    var background_player: AudioStreamPlayer = \
-        MusicPlayer.get_player(MusicPlayer.Music.FACTORY_BACKGROUND)
+    _set_fade_music(fade_music)
 
     _fade_tween.remove_all()
     _fade_tween.interpolate_property(
         _black_overlay, 'modulate:a', old, new, duration, Tween.TRANS_LINEAR,
         Tween.EASE_IN, delay)
     _fade_tween.start()
+
+func _set_fade_music(fade_music: bool) -> void:
+    if fade_music:
+        _fade_tween.connect('tween_step', self, '_on_tween_step')
+    else:
+        _fade_tween.disconnect('tween_step', self, '_on_tween_step')
 
 func _on_tween_step(obj, key, elapsed, val: float) -> void:
     # Convert current music bus volume from db to linear so that we can fade the
