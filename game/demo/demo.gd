220f50d254547da65a2ee1adfc0b921b55b24c59
Federico Menozzi <federicogmenozzi@gmail.com>
2021-03-14 09:56:34 -0700

    Save game after completing demo.
    This will ensure that the player at least has a save file created in the
    event that they blast through the demo without resting at a lamp or
    dying.

--- game/demo/demo.gd
+++ game/demo/demo.gd
@@ -224,5 +224,7 @@ func _on_end_of_demo_reached() -> void:
     # Prevent player from making any more sounds.
     player.get_sound_manager().set_all_muted(true)
 
+    _maybe_save_game()
+
     var fade_in_delay := 2.0
     SceneChanger.change_scene_to(EndOfDemoMessage, fade_in_delay)
