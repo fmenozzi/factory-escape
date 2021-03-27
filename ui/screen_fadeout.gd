422b5d0cf4b40640cdba85e552f7b222a02f17c7
Federico Menozzi <federicogmenozzi@gmail.com>
2021-03-27 12:27:19 -0700

    Fade out effects volume too during screen fadeout.
    This also helps mitigate sound effects continuing to play when e.g.
    dying in an arena full of enemies.

--- ui/screen_fadeout.gd
+++ ui/screen_fadeout.gd
@@ -9,8 +9,6 @@ const OPAQUE_ALPHA := 1.0
 onready var _black_overlay: ColorRect = $BlackOverlay
 onready var _fade_tween: Tween = $FadeTween
 
-var _global_music_slider_value: int
-
 func fade_to_black(duration: float, delay: float = 0.0, fade_music: bool = true) -> void:
     _fade(TRANSPARENT_ALPHA, OPAQUE_ALPHA, duration, delay, fade_music)
     yield(_fade_tween, 'tween_all_completed')
@@ -24,8 +22,6 @@ func fade_from_black(duration: float, delay: float = 0.0, fade_music: bool = tru
     emit_signal('fade_from_black_finished')
 
 func _fade(old: float, new: float, duration: float, delay: float, fade_music: bool) -> void:
-    _global_music_slider_value = Options.get_config().get_value('audio', 'music', 10)
-
     _set_fade_music(fade_music)
 
     _fade_tween.remove_all()
@@ -41,7 +37,8 @@ func _set_fade_music(fade_music: bool) -> void:
         _fade_tween.disconnect('tween_step', self, '_on_tween_step')
 
 func _on_tween_step(obj, key, elapsed, val: float) -> void:
-    # Convert current music bus volume from db to linear so that we can fade the
-    # music at the same rate as the screen. Take advantage of the fact that the
-    # val here (i.e. the black overlay alpha) is already in [0, 1].
-    Audio.set_bus_volume_linear('Music', (1.0 - val) * _global_music_slider_value / 10.0)
+    # Fade music/effects at the same rate as the screen. Take advantage of the
+    # fact that the value here (i.e. the black overlay alpha) is already in
+    # [0, 1].
+    Audio.set_bus_volume_linear('Music', 1.0 - val)
+    Audio.set_bus_volume_linear('Effects', 1.0 - val)
