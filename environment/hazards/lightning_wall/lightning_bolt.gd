ba605623294e391cddd637c09bbfdd40d2afb106
Federico Menozzi <federicogmenozzi@gmail.com>
2021-04-03 09:48:34 -0700

    Alternate initial y-perturb sign in LightningBolt.

--- environment/hazards/lightning_wall/lightning_bolt.gd
+++ environment/hazards/lightning_wall/lightning_bolt.gd
@@ -7,8 +7,11 @@ export(int) var num_segments := 8
 
 onready var _line: Line2D = $Line2D
 
+var _starting_sign: float
+
 func _ready() -> void:
     randomize()
+    _starting_sign = sign(rand_range(-1.0, 1.0))
 
     _line.width = width
 
@@ -19,6 +22,8 @@ func _update_points() -> void:
     var length := length_tiles * Util.TILE_SIZE
     var segment_length := length / float(num_segments)
 
+    var sgn := _starting_sign
+
     for i in range(1, num_segments):
         # Initial segment, distributed uniformly on x-axis.
         var point := Vector2(length * (i / float(num_segments)), 0.0)
@@ -28,13 +33,11 @@ func _update_points() -> void:
         var x_perturb := rand_range(-0.3 * segment_length, 0.3 * segment_length)
 
         # Perturb along y-axis, making sure to alternate sign each iteration.
-        var y_perturb := 4.0 * _alternate_sign(i)
+        var y_perturb := 4.0 * sgn
+        sgn *= -1
 
         points.append(point + Vector2(x_perturb, y_perturb))
 
     points.append(Vector2(length, 0.0))
 
     _line.points = points
-
-func _alternate_sign(i: int) -> float:
-    return 1.0 if (i % 2 == 0) else -1.0
