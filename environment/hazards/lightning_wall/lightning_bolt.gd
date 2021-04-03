bfe31b8776a3dbdfffdcf468a364034c7f0b8793
Federico Menozzi <federicogmenozzi@gmail.com>
2021-04-03 10:41:03 -0700

    Add more variation in LightningBolt perturb.
    Make max y-perturb configurable.

--- environment/hazards/lightning_wall/lightning_bolt.gd
+++ environment/hazards/lightning_wall/lightning_bolt.gd
@@ -5,6 +5,7 @@ export(Color) var color := Color('#ff4f78')
 export(float) var length_tiles := 4.0
 export(float) var width := 1.0
 export(int) var num_segments := 8
+export(float) var max_y_perturb := 4.0
 
 onready var _line: Line2D = $Line2D
 onready var _timer: Timer = $Timer
@@ -20,7 +21,7 @@ func _ready() -> void:
     _line.default_color = color
 
     _timer.connect('timeout', self, '_on_timeout')
-    _timer.start(0.05)
+    _timer.start(rand_range(0.01, 0.05))
 
 func _update_points() -> void:
     var length := length_tiles * Util.TILE_SIZE
@@ -36,10 +37,10 @@ func _update_points() -> void:
 
         # Perturb along x-axis, making sure not to overlap with neighboring
         # points.
-        var x_perturb := rand_range(-0.3 * segment_length, 0.3 * segment_length)
+        var x_perturb := rand_range(-0.4 * segment_length, 0.4 * segment_length)
 
         # Perturb along y-axis, making sure to alternate sign each iteration.
-        var y_perturb := 4.0 * sgn
+        var y_perturb := rand_range(0.0, max_y_perturb) * sgn
         sgn *= -1
 
         _points.append(point + Vector2(x_perturb, y_perturb))
