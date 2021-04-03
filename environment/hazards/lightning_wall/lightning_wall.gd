cfcaf9fcf7846f831994e5f00800bb94c863c346
Federico Menozzi <federicogmenozzi@gmail.com>
2021-04-03 13:32:31 -0700

    Add sprites + collision to LightningWall emitters.

--- environment/hazards/lightning_wall/lightning_wall.gd
+++ environment/hazards/lightning_wall/lightning_wall.gd
@@ -4,7 +4,9 @@ class_name LightningWall
 export(float) var hitbox_width := 4.0
 
 onready var _base_emitter: Node2D = $BaseEmitter
+onready var _base_emitter_bolts: Node2D = $BaseEmitter/Bolts
 onready var _movable_emitter: Node2D = $MovableEmitter
+onready var _movable_emitter_bolts: Node2D = $MovableEmitter/Bolts
 onready var _hitbox_collision_shape: CollisionShape2D = $Hitbox/CollisionShape2D
 
 func _ready() -> void:
@@ -21,10 +23,11 @@ func _ready() -> void:
     _hitbox_collision_shape.rotation = _movable_emitter.position.angle()
     _hitbox_collision_shape.position = _movable_emitter.position / 2.0
 
-    for bolt in _base_emitter.get_children():
-        bolt.length = bolt_length
-    for bolt in _movable_emitter.get_children():
-        bolt.length = bolt_length
+    # Make sure to account for the offset of the Bolts subchild in each emitter.
+    for bolt in _base_emitter_bolts.get_children():
+        bolt.length = bolt_length - (2 * _base_emitter_bolts.position.length())
+    for bolt in _movable_emitter_bolts.get_children():
+        bolt.length = bolt_length- (2 * _movable_emitter_bolts.position.length())
 
 func pause() -> void:
     pass
