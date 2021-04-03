4a40c68dbebf2a5c736f0a6e3a0cb5dc73c8298b
Federico Menozzi <federicogmenozzi@gmail.com>
2021-04-03 15:42:38 -0700

    Add fade area to Switch.

--- environment/switch/switch.gd
+++ environment/switch/switch.gd
@@ -6,15 +6,32 @@ signal state_changed(new_state)
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 onready var _fade_in_out_label: Label = $FadeInOutLabel
 onready var _label_area: Area2D = $LabelArea
+onready var _player: Player = Util.get_player()
 
 enum State {
     PRESSED,
     UNPRESSED,
 }
 
+var _state: int = State.UNPRESSED
+
 func _ready() -> void:
     _label_area.connect('body_entered', self, '_on_player_entered')
     _label_area.connect('body_exited', self, '_on_player_exited')
+
+    change_state(State.UNPRESSED)
+
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed('player_interact'):
+        if _player.get_nearby_switch() != self:
+            return
+
+        # Only allow interacting with switches while idle or walking near them.
+        if not _player.current_state() in [Player.State.IDLE, Player.State.WALK]:
+            return
+
+        if _state == State.UNPRESSED:
+            change_state(State.PRESSED)
 
 func change_state(new_state: int) -> void:
     assert(new_state in [State.PRESSED, State.UNPRESSED])
@@ -23,8 +40,18 @@ func change_state(new_state: int) -> void:
         State.PRESSED:
             _animation_player.play('pressed')
 
+            _fade_in_out_label.fade_out()
+
+            # Once pressed, switches remain pressed (unless reset externally).
+            _label_area.disconnect('body_entered', self, '_on_player_entered')
+            _label_area.disconnect('body_exited', self, '_on_player_exited')
+
         State.UNPRESSED:
             _animation_player.play('unpressed')
+
+    _state = new_state
+
+    emit_signal('state_changed', new_state)
 
 func _on_player_entered(player: Player) -> void:
     if not player:
