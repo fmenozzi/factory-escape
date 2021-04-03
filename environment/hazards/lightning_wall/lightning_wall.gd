3ebe0ba906356dab7fc1b67d58770c267813c544
Federico Menozzi <federicogmenozzi@gmail.com>
2021-04-03 12:38:17 -0700

    Add dynamic hitbox to LightningWall.

--- environment/hazards/lightning_wall/lightning_wall.gd
+++ environment/hazards/lightning_wall/lightning_wall.gd
@@ -1,8 +1,11 @@
 extends Node2D
 class_name LightningWall
 
+export(float) var hitbox_width := 4.0
+
 onready var _base_emitter: Node2D = $BaseEmitter
 onready var _movable_emitter: Node2D = $MovableEmitter
+onready var _hitbox_collision_shape: CollisionShape2D = $Hitbox/CollisionShape2D
 
 func _ready() -> void:
     assert(_base_emitter.position == Vector2.ZERO)
@@ -12,6 +15,11 @@ func _ready() -> void:
 
     _base_emitter.rotation = _movable_emitter.position.angle()
     _movable_emitter.rotation = _movable_emitter.position.rotated(PI).angle()
+
+    _hitbox_collision_shape.shape = RectangleShape2D.new()
+    _hitbox_collision_shape.shape.extents = Vector2(bolt_length / 2.0, hitbox_width / 2.0)
+    _hitbox_collision_shape.rotation = _movable_emitter.position.angle()
+    _hitbox_collision_shape.position = _movable_emitter.position / 2.0
 
     for bolt in _base_emitter.get_children():
         bolt.length = bolt_length
