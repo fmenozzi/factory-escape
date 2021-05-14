5101179eb0155c9b2b582980e3c1df73869642c8
Federico Menozzi <federicogmenozzi@gmail.com>
2021-05-13 17:55:19 -0700

    Add Springboard skeleton and demo.

---
+++ environment/springboard/springboard.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Springboard
+
+onready var _animation_player: AnimationPlayer = $AnimationPlayer
+onready var _trigger: Area2D = $TriggerArea
+
+func _ready() -> void:
+    _trigger.connect('body_entered', self, '_on_player_entered')
+
+func _on_player_entered(player: Player) -> void:
+    if not player:
+        return
+
+    _animation_player.play('spring')
