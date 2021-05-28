f0a37fa8bff2d88ad4ffc340657a38eef694e401
Federico Menozzi <federicogmenozzi@gmail.com>
2021-05-28 15:34:27 -0700

    Create VisibilityBasedAudioGroupDemo.
    This is essentially the now-deleted AudioStreamPlayerVisibilityDemo but
    with names changed and some slight changes to make it work with the new
    VisibilityBasedAudioGroup node.

--- game/testing/visibility_based_audio_group/visibility_based_audio_group_room.gd
+++ game/testing/visibility_based_audio_group/visibility_based_audio_group_room.gd
@@ -2,13 +2,16 @@ extends Room
 
 const SPEED := Util.TILE_SIZE * 4
 
-onready var obj_vis: VisibilityNotifier2D = $Sprite/AudioStreamPlayerVisibility/ObjectVisibility
-onready var att_vis: VisibilityNotifier2D = $Sprite/AudioStreamPlayerVisibility/AttenuationVisibility
+onready var obj_vis: VisibilityNotifier2D = $Sprite/VisibilityBasedAudioGroup/ObjectVisibility
+onready var att_vis: VisibilityNotifier2D = $Sprite/VisibilityBasedAudioGroup/AttenuationVisibility
 
 onready var obj_rad: float = obj_vis.rect.size.x / 2.0
 onready var att_rad: float = att_vis.rect.size.x / 2.0
 
 var _muted := false
+
+func _ready() -> void:
+    $Sprite/VisibilityBasedAudioGroup.get_player_by_name('BeepBeep').play()
 
 func _draw() -> void:
     draw_rect(Rect2($Sprite.position - Vector2(obj_rad, obj_rad), obj_vis.rect.size), Color.red, false)
@@ -17,7 +20,7 @@ func _draw() -> void:
 func _unhandled_input(event: InputEvent) -> void:
     if event is InputEventKey and event.pressed and event.scancode == KEY_M:
         _muted = not _muted
-        $Sprite/AudioStreamPlayerVisibility.set_muted(_muted)
+        $Sprite/VisibilityBasedAudioGroup.set_muted(_muted)
 
 func _process(delta: float) -> void:
     var direction := Vector2(
