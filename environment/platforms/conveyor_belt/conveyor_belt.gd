cc60271fe8ed8d89bcd41f3d381822b7cdfe40f8
Federico Menozzi <federicogmenozzi@gmail.com>
2021-07-06 13:48:22 -0700

    Add arrows to conveyor belt sprite.
    This helps better convey the direction in which the conveyor belt moves.

--- environment/platforms/conveyor_belt/conveyor_belt.gd
+++ environment/platforms/conveyor_belt/conveyor_belt.gd
@@ -4,19 +4,14 @@ const SPEED: float = 3.0 * Util.TILE_SIZE
 
 export(Util.Direction) var direction := Util.Direction.RIGHT
 
+onready var _sprite: Sprite = $Sprite
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
     assert(direction != Util.Direction.NONE)
 
-    # Play appropriate animation and set constant linear velocity depending on
-    # conveyor belt direction. Note that the 'move' animation is for moving to
-    # the right.
-    match direction:
-        Util.Direction.LEFT:
-            _animation_player.play_backwards('move')
-            set_constant_linear_velocity(Vector2(-SPEED, 0.0))
-
-        Util.Direction.RIGHT:
-            _animation_player.play('move')
-            set_constant_linear_velocity(Vector2(SPEED, 0.0))
+    # Set constant linear velocity and flip sprite according to direction. Note
+    # that the 'move' animation is for moving to the right.
+    _animation_player.play('move')
+    _sprite.flip_h = (direction == Util.Direction.LEFT)
+    set_constant_linear_velocity(Vector2(direction * SPEED, 0.0))
