b994676e0ea16431fa5f6de2e493aa52fccfd982
Federico Menozzi <federicogmenozzi@gmail.com>
2021-07-17 09:11:57 -0700

    Add COMPUTER_TERMINAL_IDLE_BEEPING sounds.
    The original single sound was split into four layers in order to make it
    harder to detect that it was a simple loop, as the four layers are at
    "uneven" lengths and thus would take a while before they all sync up
    again.

--- environment/readable_objects/computer_terminals/computer_terminal.gd
+++ environment/readable_objects/computer_terminals/computer_terminal.gd
@@ -3,3 +3,5 @@ extends ReadableObject
 func _ready() -> void:
     ._ready()
     $AnimationPlayer.play('idle')
+    for idx in range(0, $VisibilityBasedAudioGroup/AudioPlayers.get_child_count()):
+        $VisibilityBasedAudioGroup.get_player_by_index(idx).play()
