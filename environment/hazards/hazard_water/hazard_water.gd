32301ac3d4b94dba45c15f25479c022af7708d1b
Federico Menozzi <federicogmenozzi@gmail.com>
2021-08-07 11:55:05 -0700

    Add bubble particles to hazard water.

--- environment/hazards/hazard_water/hazard_water.gd
+++ environment/hazards/hazard_water/hazard_water.gd
@@ -1,9 +1,12 @@
 extends Area2D
 class_name HazardWater
 
+const PARTICLES_PER_PIXEL := 0.75
+
 onready var _collision_shape: CollisionShape2D = $CollisionShape2D
 onready var _body_sprite: Sprite = $BodySprite
 onready var _surface_foam: TextureRect = $SurfaceFoam
+onready var _bubbles: Particles2D = $Bubbles
 
 func _ready() -> void:
     assert(_collision_shape.shape is RectangleShape2D)
@@ -24,6 +27,12 @@ func _ready() -> void:
     _surface_foam.margin_right = _collision_shape.position.x + half_size.x
     _surface_foam.margin_top = _collision_shape.position.y - half_size.y - 8
     _surface_foam.margin_bottom = _collision_shape.position.y - half_size.y + 8
+
+    # Match the position and emission box width to the collision shape, and set
+    # the number of particles emitted based on the particle density.
+    _bubbles.position = _collision_shape.position + Vector2(0, -half_size.y + 8)
+    _bubbles.process_material.emission_box_extents.x = half_size.x - 2
+    _bubbles.amount = PARTICLES_PER_PIXEL * size.x
 
 func pause() -> void:
     pass
