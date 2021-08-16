bc016be1c7f011788d582755a166359fda71b7e5
Federico Menozzi <federicogmenozzi@gmail.com>
2021-08-16 08:19:35 -0700

    Create basic Ability.
    These will be fleshed out visually in the future but for now are
    functional enough for testing.

---
+++ environment/abilities/factory_escape/ability.gd
@@ -0,0 +1,29 @@
+extends Node2D
+class_name Ability
+
+signal ability_acquired(ability)
+
+enum Kind {
+    DASH,
+    DOUBLE_JUMP,
+    WALL_JUMP,
+    GRAPPLE,
+}
+
+export(Kind) var ability := Kind.DASH
+
+onready var _trigger_area: Area2D = $TriggerArea
+
+func _ready() -> void:
+    _trigger_area.connect('body_entered', self, '_on_acquired_by_player')
+
+func _on_acquired_by_player(player: KinematicBody2D) -> void:
+    if not player:
+        return
+
+    _trigger_area.call_deferred(
+        'disconnect', 'body_entered', self, '_on_acquired_by_player')
+
+    hide()
+
+    emit_signal('ability_acquired', ability)
