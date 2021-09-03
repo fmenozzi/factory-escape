0ab9b8b1b16f0e05d6e2932fc819861af9cb9af6
Federico Menozzi <federicogmenozzi@gmail.com>
2021-09-02 17:36:02 -0700

    Fix quit to menu audio bug.
    Take into account the current music/effects volume when tweening volume
    in ScreenFadeout. Previously, the tween would always start at max bus
    volume instead of current volume, so the music (which was already
    reduced in volume in the pause menu) would get louder before fading out.

--- ui/screen_fadeout.gd
+++ ui/screen_fadeout.gd
@@ -9,13 +9,22 @@ const OPAQUE_ALPHA := 1.0
 onready var _black_overlay: ColorRect = $BlackOverlay
 onready var _fade_tween: Tween = $FadeTween
 
+var _volume_multiplier_music := 1.0
+var _volume_multiplier_effects := 1.0
+
 func fade_to_black(duration: float, delay: float = 0.0, fade_music: bool = true) -> void:
+    _volume_multiplier_music = Audio.get_bus_volume_linear('Music')
+    _volume_multiplier_effects = Audio.get_bus_volume_linear('Effects')
+
     _fade(TRANSPARENT_ALPHA, OPAQUE_ALPHA, duration, delay, fade_music)
     yield(_fade_tween, 'tween_all_completed')
 
     emit_signal('fade_to_black_finished')
 
 func fade_from_black(duration: float, delay: float = 0.0, fade_music: bool = true) -> void:
+    _volume_multiplier_music = Audio.get_bus_max_volume_linear('Music')
+    _volume_multiplier_effects = Audio.get_bus_max_volume_linear('Effects')
+
     _fade(OPAQUE_ALPHA, TRANSPARENT_ALPHA, duration, delay, fade_music)
     yield(_fade_tween, 'tween_all_completed')
 
@@ -40,5 +49,10 @@ func _on_tween_step(obj, key, elapsed, val: float) -> void:
     # Fade music/effects at the same rate as the screen. Take advantage of the
     # fact that the value here (i.e. the black overlay alpha) is already in
     # [0, 1].
-    Audio.set_bus_volume_linear('Music', 1.0 - val)
-    Audio.set_bus_volume_linear('Effects', 1.0 - val)
+    Audio.set_bus_volume_linear('Music', _get_volume_linear(val, 'Music'))
+    Audio.set_bus_volume_linear('Effects', _get_volume_linear(val, 'Effects'))
+
+func _get_volume_linear(alpha: float, bus: String) -> float:
+    var multiplier := _volume_multiplier_music if bus == 'Music' else _volume_multiplier_effects
+
+    return (1.0 - alpha) * multiplier
