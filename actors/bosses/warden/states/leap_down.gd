b5a52a33dc79220b985e5b8d27dffb403577182b
Federico Menozzi <federicogmenozzi@gmail.com>
2021-09-27 08:57:14 -0700

    Add leap sequence to warden.

---
+++ actors/bosses/warden/states/leap_down.gd
@@ -0,0 +1,28 @@
+extends 'res://actors/enemies/enemy_state.gd'
+
+var _velocity := Vector2.ZERO
+
+func enter(warden: Warden, previous_state_dict: Dictionary) -> void:
+    _velocity.y = 0.0
+
+    warden.get_animation_player().play('fall')
+
+func exit(warden: Warden) -> void:
+    pass
+
+func update(warden: Warden, delta: float) -> Dictionary:
+    if warden.is_on_floor():
+        return {'new_state': Warden.State.NEXT_STATE_IN_SEQUENCE}
+
+    var physics_manager := warden.get_physics_manager()
+    var speed := physics_manager.get_horizontal_jump_speed()
+    _velocity.x = speed * warden.direction
+
+    # Fall due to gravity.
+    var gravity := physics_manager.get_gravity()
+    var terminal_velocity := physics_manager.get_terminal_velocity()
+    _velocity.y = min(_velocity.y + gravity * delta, terminal_velocity)
+
+    warden.move(_velocity)
+
+    return {'new_state': Warden.State.NO_CHANGE}
--- actors/bosses/warden/states/leap_takeoff.gd
+++ actors/bosses/warden/states/leap_takeoff.gd
@@ -1,13 +1,15 @@
 extends 'res://actors/enemies/enemy_state.gd'
 
 func enter(warden: Warden, previous_state_dict: Dictionary) -> void:
-    warden.get_animation_player().play('land')
+    warden.get_animation_player().play('jump')
+
+    warden.set_direction(Util.direction(warden, Util.get_player()))
 
 func exit(warden: Warden) -> void:
     pass
 
 func update(warden: Warden, delta: float) -> Dictionary:
     if not warden.get_animation_player().is_playing():
-        return {'new_state': Warden.State.LEAP}
+        return {'new_state': Warden.State.NEXT_STATE_IN_SEQUENCE}
 
     return {'new_state': Warden.State.NO_CHANGE}
