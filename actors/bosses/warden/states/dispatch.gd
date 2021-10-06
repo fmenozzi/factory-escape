cede59f7561d8d56027794a9d6272f24369699e9
Federico Menozzi <federicogmenozzi@gmail.com>
2021-10-06 08:31:52 -0700

    Improve warden RNG table API.
    The RNG table now specifies probabilities (as integers in [1,100])
    instead of ranges, and a different version is constructed from that that
    uses ranges for actually querying for attacks.

--- actors/bosses/warden/states/dispatch.gd
+++ actors/bosses/warden/states/dispatch.gd
@@ -1,20 +1,36 @@
 extends 'res://actors/enemies/enemy_state.gd'
 
-onready var _rng_table := {
-    range(1, 11):   Warden.State.BACKSTEP,
-    range(11, 21):  Warden.State.LEAP_TO_CENTER,
-    range(21, 31):  Warden.State.CHARGE,
-    range(31, 51):  Warden.State.SPAWN_PROJECTILES,
-    range(51, 71):  Warden.State.COMBO_LIGHTNING_FLOOR,
-    range(71, 81):  Warden.State.COMBO_LEAP,
-    range(81, 91):  Warden.State.COMBO_CHARGE_BACKSTEP,
-    range(91, 101): Warden.State.COMBO_CHARGE_LEAP_TO_CENTER,
+onready var rng_table := {
+    Warden.State.BACKSTEP:                    10,
+    Warden.State.LEAP_TO_CENTER:              10,
+    Warden.State.CHARGE:                      10,
+    Warden.State.SPAWN_PROJECTILES:           20,
+    Warden.State.COMBO_LIGHTNING_FLOOR:       20,
+    Warden.State.COMBO_LEAP:                  10,
+    Warden.State.COMBO_CHARGE_BACKSTEP:       10,
+    Warden.State.COMBO_CHARGE_LEAP_TO_CENTER: 10,
 }
 
 onready var _rng := RandomNumberGenerator.new()
 
+var _range_table := {}
+
 func _ready() -> void:
     _rng.randomize()
+
+    # Ensure probabilities add up to 100.
+    var rng_sum := 0
+    for rng_value in rng_table.values():
+        rng_sum += rng_value
+    assert(rng_sum == 100)
+
+    # Construct the range table (used for selecting attacks from a random int)
+    # from the rng table probability values.
+    var range_start := 1
+    for state in rng_table:
+        var rng_value: int = rng_table[state]
+        _range_table[range(range_start, range_start + rng_value)] = state
+        range_start += rng_value
 
 func enter(warden: Warden, previous_state_dict: Dictionary) -> void:
     pass
@@ -24,8 +40,8 @@ func exit(warden: Warden) -> void:
 
 func update(warden: Warden, delta: float) -> Dictionary:
     var rng_value := _rng.randi_range(1, 100)
-    for rng_range in _rng_table:
+    for rng_range in _range_table:
         if rng_value in rng_range:
-            return {'new_state': _rng_table[rng_range]}
+            return {'new_state': _range_table[rng_range]}
 
     return {'new_state': Warden.State.NO_CHANGE}
