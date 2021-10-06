520b27eaa1ad54ad6745ffc69eea31ecdbe54a1a
Federico Menozzi <federicogmenozzi@gmail.com>
2021-10-06 07:52:00 -0700

    Use RNG table to dispatch warden attacks.
    This table allows using custom probabilities for each attack, rather
    than enforcing that they all appear uniformly.

--- actors/bosses/warden/states/dispatch.gd
+++ actors/bosses/warden/states/dispatch.gd
@@ -1,6 +1,20 @@
 extends 'res://actors/enemies/enemy_state.gd'
 
-var _rng := RandomNumberGenerator.new()
+onready var _rng_table := {
+    range(1, 11):   Warden.State.BACKSTEP,
+    range(11, 21):  Warden.State.LEAP_TO_CENTER,
+    range(21, 31):  Warden.State.CHARGE,
+    range(31, 51):  Warden.State.SPAWN_PROJECTILES,
+    range(51, 71):  Warden.State.COMBO_LIGHTNING_FLOOR,
+    range(71, 81):  Warden.State.COMBO_LEAP,
+    range(81, 91):  Warden.State.COMBO_CHARGE_BACKSTEP,
+    range(91, 101): Warden.State.COMBO_CHARGE_LEAP_TO_CENTER,
+}
+
+onready var _rng := RandomNumberGenerator.new()
+
+func _ready() -> void:
+    _rng.randomize()
 
 func enter(warden: Warden, previous_state_dict: Dictionary) -> void:
     pass
@@ -9,22 +23,9 @@ func exit(warden: Warden) -> void:
     pass
 
 func update(warden: Warden, delta: float) -> Dictionary:
-    match _rng.randi_range(1, 8):
-        1:
-            return {'new_state': Warden.State.BACKSTEP}
-        2:
-            return {'new_state': Warden.State.COMBO_LEAP}
-        3:
-            return {'new_state': Warden.State.LEAP_TO_CENTER}
-        4:
-            return {'new_state': Warden.State.CHARGE}
-        5:
-            return {'new_state': Warden.State.COMBO_LIGHTNING_FLOOR}
-        6:
-            return {'new_state': Warden.State.SPAWN_PROJECTILES}
-        7:
-            return {'new_state': Warden.State.COMBO_CHARGE_BACKSTEP}
-        8:
-            return {'new_state': Warden.State.COMBO_CHARGE_LEAP_TO_CENTER}
+    var rng_value := _rng.randi_range(1, 100)
+    for rng_range in _rng_table:
+        if rng_value in rng_range:
+            return {'new_state': _rng_table[rng_range]}
 
     return {'new_state': Warden.State.NO_CHANGE}
