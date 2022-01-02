e3da5a1907350a19e22dd107c975917e74e37100
Federico Menozzi <federicogmenozzi@gmail.com>
2022-01-02 08:07:12 -0800

    Slightly improve escape sequence start.
    While not final, this should be closer to the final version (i.e. with
    the closing doors and the switch that controls when to "overload" the
    system and initiate the self-destruct sequence).

--- game/factory_escape/rooms/sector_5/sector_five_13.gd
+++ game/factory_escape/rooms/sector_5/sector_five_13.gd
@@ -1,10 +1,21 @@
 extends Room
 
-func _ready() -> void:
-    $EscapeSequenceTrigger.connect('body_entered', self, '_start_escape_sequence')
+onready var _closing_door_left: StaticBody2D = $ClosingDoorLeft
+onready var _closing_door_right: StaticBody2D = $ClosingDoorRight
+onready var _switch: Switch = $Switch
 
-func _start_escape_sequence(player: Player) -> void:
-    if not player:
-        return
+func _ready() -> void:
+    lamp_reset()
+
+    _switch.connect('switch_press_finished', self, '_start_escape_sequence')
+
+func _start_escape_sequence() -> void:
+    _closing_door_left.close()
+    _closing_door_right.open()
 
     EscapeSequenceEffects.start()
+
+func lamp_reset() -> void:
+    _closing_door_left.set_opened()
+    _closing_door_right.set_closed()
+    _switch.reset_state_to(Switch.State.UNPRESSED)
