d98991aceb04bf14cefd2faa74b89122ae1c134e
Federico Menozzi <federicogmenozzi@gmail.com>
2022-01-18 08:10:52 -0800

    Fix bug where debris would queue free on pause.
    This fixes #65

--- custom/nodes/escape_sequence/debris.gd
+++ custom/nodes/escape_sequence/debris.gd
@@ -1,6 +1,12 @@
 extends Particles2D
 
+onready var _timer: Timer = $Timer
+
+func _ready() -> void:
+    _timer.one_shot = true
+    _timer.wait_time = 2 * lifetime
+    _timer.connect('timeout', self, 'queue_free')
+
 func start_and_queue_free() -> void:
     emitting = true
-    yield(get_tree().create_timer(lifetime * 2), 'timeout')
-    queue_free()
+    _timer.start()
